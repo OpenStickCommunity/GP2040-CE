c35205a0cf8adb2d888baffd901980cc3a85e536
FeralAI <13342258+FeralAI@users.noreply.github.com>
2021-10-12 01:53:58 -0500

    Fix loading settings for old boards

--- lib/FlashPROM/src/FlashPROM.cpp
+++ lib/FlashPROM/src/FlashPROM.cpp
@@ -8,7 +8,7 @@
 volatile static alarm_id_t flashWriteAlarm = 0;
 static spin_lock_t *flashLock = nullptr;
 
-int64_t writeToFlash(alarm_id_t id, void *cache)
+int64_t writeToFlash(alarm_id_t id, void *flashCache)
 {
 	multicore_lockout_start_blocking();
 
@@ -16,7 +16,7 @@ int64_t writeToFlash(alarm_id_t id, void *cache)
 	uint32_t interrupts = spin_lock_blocking(flashLock);
 
 	flash_range_erase((intptr_t)EEPROM_ADDRESS_START - (intptr_t)XIP_BASE, EEPROM_SIZE_BYTES);
-	flash_range_program((intptr_t)EEPROM_ADDRESS_START - (intptr_t)XIP_BASE, reinterpret_cast<uint8_t *>(cache), EEPROM_SIZE_BYTES);
+	flash_range_program((intptr_t)EEPROM_ADDRESS_START - (intptr_t)XIP_BASE, reinterpret_cast<uint8_t *>(flashCache), EEPROM_SIZE_BYTES);
 
 	spin_unlock(flashLock, interrupts);
 
@@ -32,8 +32,24 @@ void FlashPROM::start()
 		flashLock = spin_lock_instance(spin_lock_claim_unused(true));
 
 	memcpy(cache, reinterpret_cast<uint8_t *>(EEPROM_ADDRESS_START), EEPROM_SIZE_BYTES);
+
+	// When flash is new/reset, all bits are set to 1.
+	// If all bits from the FlashPROM section are 1's then set to 0's.
+	bool reset = true;
 	for (int i = 0; i < EEPROM_SIZE_BYTES; i++)
-		cache[i] = ~cache[i];
+	{
+		if (cache[i] != 0xFF)
+		{
+			reset = false;
+			break;
+		}
+	}
+
+	if (reset)
+	{
+		memset(cache, 0, EEPROM_SIZE_BYTES);
+		commit();
+	}
 }
 
 /* We don't have an actual EEPROM, so we need to be extra careful about minimizing writes. Instead
@@ -50,9 +66,5 @@ void FlashPROM::commit()
 		while (is_spin_locked(flashLock));
 	}
 
-	static uint8_t flashCache[EEPROM_SIZE_BYTES];
-	for (int i = 0; i < EEPROM_SIZE_BYTES; i++)
-		flashCache[i] = ~cache[i];
-
-	flashWriteAlarm = add_alarm_in_ms(EEPROM_WRITE_WAIT, writeToFlash, flashCache, true);
+	flashWriteAlarm = add_alarm_in_ms(EEPROM_WRITE_WAIT, writeToFlash, cache, true);
 }
