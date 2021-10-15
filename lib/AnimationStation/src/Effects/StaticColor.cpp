0c28290f852c5b9d23e91d1bb361b761683bb8d3
FeralAI <13342258+FeralAI@users.noreply.github.com>
2021-10-15 00:37:09 -0500

    Refactor pixel iterators

--- lib/AnimationStation/src/Effects/StaticColor.cpp
+++ lib/AnimationStation/src/Effects/StaticColor.cpp
@@ -1,18 +1,17 @@
-#include "StaticColor.hpp"
+ #include "StaticColor.hpp"
 
 StaticColor::StaticColor(PixelMatrix &matrix, RGB color) : Animation(matrix), color(color) {
 
 }
 
 void StaticColor::Animate(RGB (&frame)[100]) {
-  for (size_t r = 0; r != matrix->pixels.size(); r++) {
-    for (size_t c = 0; c != matrix->pixels[r].size(); c++) {
-      if (matrix->pixels[r][c].index == NO_PIXEL.index)
+  for (auto &col : matrix->pixels) {
+    for (auto &pixel : col) {
+      if (pixel.index == NO_PIXEL.index)
         continue;
 
-      for (size_t p = 0; p != matrix->pixels[r][c].positions.size(); p++) {
-        frame[matrix->pixels[r][c].positions[p]] = color;
-      }
+      for (auto &pos : pixel.positions)
+        frame[pos] = color;
     }
   }
 }
--- lib/AnimationStation/src/Effects/StaticTheme.cpp
+++ lib/AnimationStation/src/Effects/StaticTheme.cpp
@@ -4,21 +4,19 @@ StaticTheme::StaticTheme(PixelMatrix &matrix, std::map<uint32_t, RGB> theme, RGB
   : Animation(matrix), theme(theme), defaultColor(defaultColor) { }
 
 void StaticTheme::Animate(RGB (&frame)[100]) {
-  for (size_t r = 0; r != matrix->pixels.size(); r++) {
-    for (size_t c = 0; c != matrix->pixels[r].size(); c++) {
-      if (matrix->pixels[r][c].index == NO_PIXEL.index)
+  for (auto &col : matrix->pixels) {
+    for (auto &pixel : col) {
+      if (pixel.index == NO_PIXEL.index)
         continue;
 
-      auto itr = theme.find(matrix->pixels[r][c].mask);
+      auto itr = theme.find(pixel.mask);
       if (itr != theme.end()) {
-        for (size_t p = 0; p != matrix->pixels[r][c].positions.size(); p++) {
-          frame[matrix->pixels[r][c].positions[p]] = itr->second;
-        }
+        for (auto &pos : pixel.positions)
+          frame[pos] = itr->second;
       }
       else {
-        for (size_t p = 0; p != matrix->pixels[r][c].positions.size(); p++) {
-          frame[matrix->pixels[r][c].positions[p]] = defaultColor;
-        }
+        for (auto &pos : pixel.positions)
+          frame[pos] = defaultColor;
       }
     }
   }
