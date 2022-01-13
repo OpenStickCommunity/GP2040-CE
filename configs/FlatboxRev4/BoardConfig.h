fdb17d4d20cfcb7ac603647be0f876a245d198fc
FeralAI <13342258+FeralAI@users.noreply.github.com>
2022-01-12 23:23:01 -0600

    Add Flatbox rev 4 build

---
+++ configs/FlatboxRev4/BoardConfig.h
@@ -0,0 +1,33 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2021 Jason Skuby (mytechtoybox.com)
+ */
+
+#ifndef FLATBOX_REV4_CONFIG_H_
+#define FLATBOX_REV4_CONFIG_H_
+
+#include <GamepadEnums.h>
+
+#define PIN_DPAD_UP     16
+#define PIN_DPAD_DOWN   10
+#define PIN_DPAD_LEFT   9
+#define PIN_DPAD_RIGHT  11
+#define PIN_BUTTON_B1   19
+#define PIN_BUTTON_B2   24
+#define PIN_BUTTON_B3   18
+#define PIN_BUTTON_B4   25
+#define PIN_BUTTON_L1   29
+#define PIN_BUTTON_R1   27
+#define PIN_BUTTON_L2   28
+#define PIN_BUTTON_R2   26
+#define PIN_BUTTON_S1   3
+#define PIN_BUTTON_S2   1
+#define PIN_BUTTON_L3   6
+#define PIN_BUTTON_R3   4
+#define PIN_BUTTON_A1   2
+#define PIN_BUTTON_A2   5
+
+#define DEFAULT_SOCD_MODE SOCD_MODE_UP_PRIORITY
+#define BUTTON_LAYOUT BUTTON_LAYOUT_HITBOX
+
+#endif
