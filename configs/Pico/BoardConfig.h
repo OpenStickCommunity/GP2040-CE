b5ad24ccfbd850d43e93aa45f7d11aef4ff453ee
TheTrain <32771064+TheTrainGoes@users.noreply.github.com>
2022-08-21 23:11:07 -0400

    Update BoardConfig.h

--- configs/Pico/BoardConfig.h
+++ configs/Pico/BoardConfig.h
@@ -8,6 +8,14 @@
 
 #include <GamepadEnums.h>
 
+
+// This is the main pin definition section.
+// This will let you specify which GPIO pin each button is assigned to. 
+// You can set any of the main pins as `-1` to disable it.
+// The Turbo pin and LS + RS slider pins can also be set to `-1` to disable that functionality.
+// Please note that only when `PIN_BUTTON_TURBO` is set to `-1` will the `T##` be removed from a connected display.
+// Please note that only when `PIN_SLIDER_LS` and  `PIN_SLIDER_RS` are set to `-1` will the button combo shortcut work.
+
 #define PIN_DPAD_UP     2
 #define PIN_DPAD_DOWN   3
 #define PIN_DPAD_RIGHT  4
@@ -30,9 +38,10 @@
 #define PIN_SLIDER_LS    -1
 #define PIN_SLIDER_RS    -1
 
+
+// This is the main pin definition section.
+
 #define DEFAULT_SOCD_MODE SOCD_MODE_NEUTRAL
-#define BUTTON_LAYOUT BUTTON_LAYOUT_STICK
-#define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_VEWLIX
 
 #define SPLASH_MODE STATICSPLASH
 
@@ -58,19 +67,73 @@
 #define LEDS_BUTTON_R2   10
 #define LEDS_BUTTON_L2   11
 
+// This is the Player LED section.  
+// In this section you can specify if Player LEDs will be active, and, if active, which pins will be used for them.
+// The defualt is `PLED_TYPE_NONE` which will turn the Player LEDs off.
+// The default pin for each Player LED is `-1` which disables it.  
+// To enable a `PLED#_PIN`, replace the `-1` with the GPIO pin number that is desired. 
+// There are three options for `PLED_TYPE` currently:
+// 1 - `PLED_TYPE_NONE` - This will disable the Player LEDs
+// 2 - `PLED_TYPE_PWM` - This will enable the Player LEDs ( it is recommended to run through 3V3(OUT))
+// 3 - `PLED_TYPE_RGB` - This will enable the Player LEDs as addressible RGB LEDs (please not that this has not been implemented yet) 
+
 #define PLED_TYPE PLED_TYPE_NONE
 #define PLED1_PIN -1
 #define PLED2_PIN -1
 #define PLED3_PIN -1
 #define PLED4_PIN -1
 
+
+// This is the Analog section.
+// In this section you can specify if Analog is enabled, and, if endabled, which pins will be used for it.
+// The default for `ANALOG_ADC_VRX` and `ANALOG_ADC_VRY` is `-1` which disables them.
+// To enable a `ANALOG_ADC_VRX` and `ANALOG_ADC_VRY`, replace the `-1` with the GPIO pin numbers that are desired. 
+
 #define ANALOG_ADC_VRX -1
 #define ANALOG_ADC_VRY -1
+
+
+// This is the I2C Display section (commonly known as the OLED display section).
+// In this section you can specify if a display as been enabled, which pins are assined to it, the block address and speed.
+// The default for `HAS_I2C_DISPLAY` is `1` which enables it.
+// To disable the display you can change `HAS_I2C_DISPLAY` to `-1`.
+// The default `I2C_SDA_PIN` is `0`.
+// The defualt `I2C_SCL_PIN` is `1`.
+// The defualt `I2C_BLOCK` is `12c0`.  
+// If you change the `I2C_SDA_PIN` and `I2C_SCL_PIN` pin mapping, you may need to change the `I2C_BLOCK` as well.
+// The defualt `I2C_SPEED` is `400000`.  
+// This should be more than fast enough for most displays.
+// Some smaller displays (like 0.96" and 1.31") can go up to `800000` or even `1000000`.
+// Some larger dipsays (like 2.42") should be set around `100000` to avoid artifacting issues.
+// The default `BUTTON_LAYOUT` is `BUTTON_LAYOUT_STICK` which will show an arcade stick on the left hand side of the display.
+// There are seven options for `BUTTON_LAYOUT` currently:
+// 1 - BUTTON_LAYOUT_STICK - This is a basic joystick layout
+// 2 - BUTTON_LAYOUT_STICKLESS - This is a basic stickless (all button) layout
+// 3 - BUTTON_LAYOUT_BUTTONS_ANGLED - This a WASD button layout that is angled
+// 4 - BUTTON_LAYOUT_BUTTONS_BASIC - This a WASD button layout that is straight
+// 5 - BUTTON_LAYOUT_KEYBOARD_ANGLED - This is a WASD keyboard layout that is angled
+// 6 - BUTTON_LAYOUT_KEYBOARDA - This is a WASD keyboard layout that is straight
+// 7 - BUTTON_LAYOUT_DANCEPADA - This is a dance pad layout (must be used with `BUTTON_LAYOUT_DANCEPADB` in `BUTTON_LAYOUT_RIGHT`)
+// The default `BUTTON_LAYOUT_RIGHT` is `BUTTON_LAYOUT_NOIR8` which will show eight buttons on the right hand side of the display.
+// There are eleven options for `BUTTON_LAYOUT_RIGHT` currently:
+// 1 - BUTTON_LAYOUT_ARCADE - This is a standard 8 button arcade layout
+// 2 - BUTTON_LAYOUT_STICKLESSB - This is a basic stickless (all button) layout
+// 3 - BUTTON_LAYOUT_BUTTONS_ANGLEDB - This is a standard 8 button arcade layout that is angled
+// 4 - BUTTON_LAYOUT_VEWLIX - This is the standard 8 button Vewlix layout
+// 5 - BUTTON_LAYOUT_VEWLIX7 - This is the standard 7 button Vewlix layout
+// 6 - BUTTON_LAYOUT_CAPCOM - This is the standard 8 button Capcom layout
+// 7 - BUTTON_LAYOUT_CAPCOM6 - This is the stndard 6 button Capcom layout
+// 8 - BUTTON_LAYOUT_SEGA2P - This is the standard 8 button Sega2P layout
+// 9 - BUTTON_LAYOUT_NOIR8 - This is the standard 8 button Noir layout
+// 10 - BUTTON_LAYOUT_KEYBOARDB - This is a WASD keyboard layout that is straight
+// 11 - BUTTON_LAYOUT_DANCEPADB - This is a dance pad layout (must be used with `BUTTON_LAYOUT_DANCEPADA` in `BUTTON_LAYOUT`)
 
 #define HAS_I2C_DISPLAY 1
 #define I2C_SDA_PIN 0
 #define I2C_SCL_PIN 1
 #define I2C_BLOCK i2c0
 #define I2C_SPEED 400000
+#define BUTTON_LAYOUT BUTTON_LAYOUT_STICK
+#define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_VEWLIX
 
 #endif
