5b5a734f875f9321722f371c57f6e95e6bf7b02d
Rafael Cruz <rafaelcruz@outlook.com>
2022-12-02 22:17:37 -0300

    Add tones and freq functions

--- src/addons/buzzerspeaker.cpp
+++ src/addons/buzzerspeaker.cpp
@@ -1,3 +1,11 @@
+/** 
+ * Buzzer Speaker
+ * References:
+ * https://www.i-programmer.info/programming/hardware/14849-the-pico-in-c-basic-pwm.html?start=1
+ * https://www.tomshardware.com/how-to/buzzer-music-raspberry-pi-pico
+ * 
+ * **/
+
 #include "addons/buzzerspeaker.h"
 #include "enums.h"
 #include "helper.h"
@@ -26,17 +34,38 @@ void BuzzerSpeakerAddon::setup() {
 	gpio_set_function(boardOptions.buzzerPin, GPIO_FUNC_PWM);
 	buzzerPinSlice = pwm_gpio_to_slice_num (boardOptions.buzzerPin); 
 	buzzerPinChannel = pwm_gpio_to_channel (boardOptions.buzzerPin);
+
+	int i = 1;
+
+	while (i <= 2000) {
+		pwmSetFreqDuty(buzzerPinSlice, buzzerPinChannel, i, 75);
+		pwm_set_enabled (buzzerPinSlice, true); 
+		sleep_ms(100);
+		i++;
+		if (i >= 2000) {
+			i=1;
+		}
+    }
+
+	
 }
 
 void BuzzerSpeakerAddon::process() {
 	bool configMode = Storage::getInstance().GetConfigMode();
 
-	pwm_set_freq_duty(buzzerPinSlice, buzzerPinChannel, 50, 75);
-	pwm_set_enabled (buzzerPinSlice, true); 
+}
 
-	// test
-	gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-	gpio_put(LED_PIN, 1);
+uint32_t BuzzerSpeakerAddon::pwmSetFreqDuty(uint slice_num, uint chan, uint32_t f, int d) {
+	uint32_t clock = 125000000;
+	uint32_t divider16 = clock / f / 4096 + 
+							(clock % (f * 4096) != 0);
+	if (divider16 / 16 == 0)
+	divider16 = 16;
+	uint32_t wrap = clock * 16 / divider16 / f - 1;
+	pwm_set_clkdiv_int_frac(slice_num, divider16/16,
+										divider16 & 0xF);
+	pwm_set_wrap(slice_num, wrap);
+	pwm_set_chan_level(slice_num, chan, wrap * d / 100);
+	return wrap;
 }
 
