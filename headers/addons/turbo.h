8d490139388b2d9f255cdad85c9340d16f72a6af
Luke A <arntsonl@gmail.com>
2023-03-15 11:09:03 -0400

    SHMUP mode initial commits

--- headers/addons/turbo.h
+++ headers/addons/turbo.h
@@ -20,6 +20,68 @@
 #define TURBO_LED_PIN   -1
 #endif
 
+// TURBO SHMUP MODE
+#ifndef TURBO_SHMUP_MODE
+#define TURBO_SHMUP_MODE -1
+#endif
+
+enum { 
+    TURBO_PRIORITY,
+    CHARGE_PRIORITY
+} ShmupMixMode;
+
+#ifndef SHMUP_ALWAYS_ON1
+#define SHMUP_ALWAYS_ON1 0
+#endif
+
+#ifndef SHMUP_ALWAYS_ON2
+#define SHMUP_ALWAYS_ON2 0
+#endif
+
+#ifndef SHMUP_ALWAYS_ON3
+#define SHMUP_ALWAYS_ON3 0
+#endif
+
+#ifndef SHMUP_ALWAYS_ON4
+#define SHMUP_ALWAYS_ON4 0
+#endif
+
+#ifndef PIN_SHMUP_BUTTON1
+#define PIN_SHMUP_BUTTON1 -1
+#endif
+
+#ifndef PIN_SHMUP_BUTTON2
+#define PIN_SHMUP_BUTTON2 -1
+#endif
+
+#ifndef PIN_SHMUP_BUTTON3
+#define PIN_SHMUP_BUTTON3 -1
+#endif
+
+#ifndef PIN_SHMUP_BUTTON4
+#define PIN_SHMUP_BUTTON4 -1
+#endif
+
+#ifndef SHMUP_BUTTON1
+#define SHMUP_BUTTON1 0
+#endif
+
+#ifndef SHMUP_BUTTON2
+#define SHMUP_BUTTON2 0
+#endif
+
+#ifndef SHMUP_BUTTON3
+#define SHMUP_BUTTON3 0
+#endif
+
+#ifndef SHMUP_BUTTON4
+#define SHMUP_BUTTON4 0
+#endif
+
+#ifndef PIN_SHMUP_DIAL
+#define PIN_SHMUP_DIAL -1
+#endif
+
 // Turbo Module Name
 #define TurboName "Turbo"
 
@@ -31,17 +93,21 @@ public:
 	virtual void process();     // TURBO Setting of buttons (Enable/Disable)
     virtual std::string name() { return TurboName; }
 private:
-    virtual bool read();        // Get TURBO Button State
-    virtual void debounce();    // TURBO Button Debouncer
+    bool read();        // Get TURBO Button State
+    void debounce();    // TURBO Button Debouncer
     bool bDebState;             // Debounce TURBO Button State
     uint32_t uDebTime;          // Debounce TURBO Button Time
     uint16_t lastPressed;       // Last buttons pressed (for Turbo Enable)
     uint16_t lastDpad;          // Last d-pad pressed (for Turbo Change)
     uint16_t buttonsEnabled;    // Turbo Buttons Enabled
+    uint16_t alwaysEnabled;     // Turbo SHMUP Always Enabled
     uint32_t uIntervalMS;       // Turbo Interval
     bool bTurboState;           // Turbo Buttons State
     bool bTurboFlicker;         // Turbo Enable Buttons Toggle OFF Flag ??
     uint32_t nextTimer;         // Turbo Timer
     uint8_t pinButtonTurbo;     // Turbo Button Pin
+    uint8_t shmupMode;          // Turbo SHMUP Mode
+    uint8_t shmupBtnPin[4];     // Turbo SHMUP Non-Turbo Pins
+    uint16_t shmupBtnMask[4]; // Turbo SHMUP Non-Turbo Button Masks
 };
 #endif  // TURBO_H_
--- src/addons/turbo.cpp
+++ src/addons/turbo.cpp
@@ -1,5 +1,7 @@
 #include "addons/turbo.h"
 
+#include "hardware/adc.h"
+
 #include "storagemanager.h"
 
 #define TURBO_DEBOUNCE_MILLIS 5
@@ -10,16 +12,17 @@
 bool TurboInput::available() {
 	AddonOptions options = Storage::getInstance().getAddonOptions();
     pinButtonTurbo = options.pinButtonTurbo;
-    return (options.TurboInputEnabled &&
-        pinButtonTurbo != (uint8_t)-1);
+    return options.TurboInputEnabled;
 }
 
 void TurboInput::setup()
 {
-    // Setup TURBO Key
-    gpio_init(pinButtonTurbo);             // Initialize pin
-    gpio_set_dir(pinButtonTurbo, GPIO_IN); // Set as INPUT
-    gpio_pull_up(pinButtonTurbo);          // Set as PULLUP
+    // Setup TURBO Key GPIO
+    if ( pinButtonTurbo != -1 ) {
+        gpio_init(pinButtonTurbo);             // Initialize pin
+        gpio_set_dir(pinButtonTurbo, GPIO_IN); // Set as INPUT
+        gpio_pull_up(pinButtonTurbo);          // Set as PULLUP
+    }
     
     // Setup Turbo LED if available
     AddonOptions options = Storage::getInstance().getAddonOptions();
@@ -29,11 +32,39 @@ void TurboInput::setup()
         gpio_put(options.pinTurboLED, 1);
     }
 
+    // SHMUP Mode
+    shmupMode = options.shmupMode;
+    if ( shmupMode == 1 ) {
+        shmupBtnPin[0] = options.pinShmupBtn1; // Never Turbo aux buttons
+        shmupBtnPin[1] = options.pinShmupBtn2;
+        shmupBtnPin[2] = options.pinShmupBtn3;
+        shmupBtnPin[3] = options.pinShmupBtn4;
+        for (uint8_t i = 0; i < 4; i++) {
+            if ( shmupBtnPin[i] != -1 ) {
+                gpio_init(shmupBtnPin[i]);
+                gpio_set_dir(shmupBtnPin[i], GPIO_IN);
+                gpio_pull_up(shmupBtnPin[i]);
+            }
+        }
+        shmupBtnMask[0] = options.shmupBtnMask1;
+        shmupBtnMask[1] = options.shmupBtnMask2;
+        shmupBtnMask[2] = options.shmupBtnMask3;
+        shmupBtnMask[3] = options.shmupBtnMask4;
+        alwaysEnabled = options.shmupAlwaysOn1 | options.shmupAlwaysOn2 |
+                            options.shmupAlwaysOn3 | options.shmupAlwaysOn4;
+        if ( options.pinShmupDial != -1 ) {
+            adc_gpio_init(options.pinShmupDial);
+        }
+        buttonsEnabled = alwaysEnabled;
+    } else {
+        // SHMUP mode off
+        buttonsEnabled = 0;
+    }
+
     bDebState = false;
     uDebTime = getMillis();
     lastPressed = 0;
     lastDpad = 0;
-    buttonsEnabled = 0;
     uIntervalMS = (uint32_t)(1000.0 / options.turboShotCount);
     bTurboState = false;
     bTurboFlicker = false;
@@ -42,6 +73,21 @@ void TurboInput::setup()
 
 bool TurboInput::read()
 {
+    // Get the SHMUP no turbo buttons
+    if ( shmupMode ) {
+        for (uint8_t i = 0; i < 4; i++) {
+            if ( shmupBtnPin[i] != -1 ) {
+                !gpio_get(shmupBtnPin[i]);
+            }
+        }
+
+        // Use the dial to modify our turbo shot speed
+        if ( options.pinShmupDial != -1 ) {
+            adc_select_input(26-options.pinShmupDial);
+            uint16_t dial = adc_read();
+        }
+    }
+
     // Get TURBO Key State
     return(!gpio_get(pinButtonTurbo));
 }
@@ -73,6 +119,9 @@ void TurboInput::process()
     if (bTurboState) {
         if (buttonsPressed && (lastPressed != buttonsPressed)) {
             buttonsEnabled ^= buttonsPressed; // Toggle Turbo
+            if ( options.shmupMode == 1 ) {
+                buttonsEnabled |= alwaysEnabled;  // SHMUP Always-on Buttons Set
+            }
             // Turn off button once turbo is toggled
             gamepad->state.buttons &= ~(TURBO_BUTTON_MASK);
         }
@@ -97,7 +146,6 @@ void TurboInput::process()
         lastDpad = 0; // disable last dpad
     }
 
-
     // Set TURBO LED if a button is going or turbo is too fast
     if ( options.pinTurboLED != -1 ) {
         if ((gamepad->state.buttons & buttonsEnabled) && !bTurboFlicker) {
@@ -110,6 +158,11 @@ void TurboInput::process()
     // disable button during turbo flicker
     if (bTurboFlicker) { 
         gamepad->state.buttons &= ~(buttonsEnabled);
+    }
+
+    // set non-shmup buttons after we do the flicker
+    if ( shmupMode == 1 ) {
+
     }
 
     if (getMillis() < nextTimer) {
