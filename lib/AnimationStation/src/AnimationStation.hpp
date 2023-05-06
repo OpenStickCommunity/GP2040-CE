0a150ddb6438e4daee39fd2505f81bd3ff214050
FeralAI <13342258+FeralAI@users.noreply.github.com>
2023-05-06 12:41:55 -0500

    Custom LED implementation (#166)
    * Custom LED implementation
    
    * Clean includes and set default custom LED options
    
    * Move animation options
    
    * Missed a spot
    
    * Fix memory issue with custom theme save
    
    * Fix race condition and reverse animation cycling
    
    * Reduce flicker when changing animations
    
    * Add documentation for Custom LED theme

--- lib/AnimationStation/src/AnimationStation.hpp
+++ lib/AnimationStation/src/AnimationStation.hpp
@@ -11,6 +11,8 @@
 #include "NeoPico.hpp"
 #include "Animation.hpp"
 #include "Effects/Chase.hpp"
+#include "Effects/CustomTheme.hpp"
+#include "Effects/CustomThemePressed.hpp"
 #include "Effects/Rainbow.hpp"
 #include "Effects/StaticColor.hpp"
 #include "Effects/StaticTheme.hpp"
@@ -20,11 +22,11 @@ typedef enum
   EFFECT_STATIC_COLOR,
   EFFECT_RAINBOW,
   EFFECT_CHASE,
-  EFFECT_STATIC_THEME
+  EFFECT_STATIC_THEME,
+  EFFECT_CUSTOM_THEME,
 } AnimationEffects;
 
-// We can't programmatically determine how many elements are in an enum. Yes, that's dumb.
-const int TOTAL_EFFECTS = 4;
+const int TOTAL_EFFECTS = 4; // Exclude custom theme until verified present
 
 typedef enum
 {
@@ -49,6 +51,43 @@ struct __attribute__ ((__packed__)) AnimationOptions
   int16_t chaseCycleTime;
   int16_t rainbowCycleTime;
   uint8_t themeIndex;
+  bool hasCustomTheme;
+  uint32_t customThemeUp;
+  uint32_t customThemeDown;
+  uint32_t customThemeLeft;
+  uint32_t customThemeRight;
+  uint32_t customThemeB1;
+  uint32_t customThemeB2;
+  uint32_t customThemeB3;
+  uint32_t customThemeB4;
+  uint32_t customThemeL1;
+  uint32_t customThemeR1;
+  uint32_t customThemeL2;
+  uint32_t customThemeR2;
+  uint32_t customThemeS1;
+  uint32_t customThemeS2;
+  uint32_t customThemeL3;
+  uint32_t customThemeR3;
+  uint32_t customThemeA1;
+  uint32_t customThemeA2;
+  uint32_t customThemeUpPressed;
+  uint32_t customThemeDownPressed;
+  uint32_t customThemeLeftPressed;
+  uint32_t customThemeRightPressed;
+  uint32_t customThemeB1Pressed;
+  uint32_t customThemeB2Pressed;
+  uint32_t customThemeB3Pressed;
+  uint32_t customThemeB4Pressed;
+  uint32_t customThemeL1Pressed;
+  uint32_t customThemeR1Pressed;
+  uint32_t customThemeL2Pressed;
+  uint32_t customThemeR2Pressed;
+  uint32_t customThemeS1Pressed;
+  uint32_t customThemeS2Pressed;
+  uint32_t customThemeL3Pressed;
+  uint32_t customThemeR3Pressed;
+  uint32_t customThemeA1Pressed;
+  uint32_t customThemeA2Pressed;
 };
 
 class AnimationStation
@@ -81,6 +120,7 @@ public:
   std::vector<Pixel> lastPressed;
   static AnimationOptions options;
   static absolute_time_t nextChange;
+  static uint8_t effectCount;
   RGB frame[100];
 
 protected:
