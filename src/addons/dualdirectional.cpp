875d72c3676396f3647f79009b4156b487945c2f
Brian S. Stephan <bss@incorporeal.org>
2023-05-06 18:41:48 -0500

    assign dual inputs to gamepad in Combination Mode None
    process() was totally ignoring DUAL_COMBINE_MODE_NONE. this takes the
    dual inputs and assigns them to whatever is configured under Dual D-Pad
    Mode. this has been SOCD cleaned in aa1f8696
    
    note that this assignment is a direct override when Dual D-Pad Mode is
    *different* than the current "normal" D-Pad Mode (as their inputs
    contribute to different fields), but if they are the same, the two
    inputs contribute in essentially the same as Mixed mode and need to be
    SOCD cleaned.

--- src/addons/dualdirectional.cpp
+++ src/addons/dualdirectional.cpp
@@ -121,6 +121,7 @@ void DualDirectionalInput::preprocess()
 
 void DualDirectionalInput::process()
 {
+    const AddonOptions& options = Storage::getInstance().getAddonOptions();
     Gamepad * gamepad = Storage::getInstance().GetGamepad();
     uint8_t dualOut = dualState;
     const SOCDMode& socdMode = getSOCDMode(gamepad->options);
@@ -148,6 +149,17 @@ void DualDirectionalInput::process()
         if ( combineMode == DUAL_COMBINE_MODE_GAMEPAD ) {
             // Set Dual Directional Output
             OverrideGamepad(gamepad, dpadMode, dualOut);
+        }
+        else if (combineMode == DUAL_COMBINE_MODE_NONE) {
+            // Set the configured directional mode to the value of the dual output
+            // if configured dual mode is the same mode as the gamepad mode, they
+            // need to be SOCD cleaned first
+            // this also avoids accidentally masking gamepad inputs with the lack of dual inputs
+            if (gamepad->options.dpadMode == options.dualDirDpadMode) {
+                uint8_t gamepadDpad = gpadToBinary(gamepad->options.dpadMode, gamepad->state);
+                dualOut = SOCDGamepadClean(dualOut | gamepadDpad, gamepad->options.socdMode == SOCD_MODE_SECOND_INPUT_PRIORITY);
+            }
+            OverrideGamepad(gamepad, options.dualDirDpadMode, dualOut);
         }
     }
 }
