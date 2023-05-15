09125803ae3708a35be45b34f0371cee6ec51b1a
Robert Dale Smith <robert@robertdalesmith.com>
2023-05-15 16:10:39 -0500

    adds webconfiguration options for SNESpad controller input

--- www/server/app.js
+++ www/server/app.js
@@ -222,6 +222,9 @@ app.get("/api/getAddonsOptions", (req, res) => {
 		wiiExtensionSCLPin: -1,
 		wiiExtensionBlock: 0,
 		wiiExtensionSpeed: 400000,
+		snesPadClockPin: -1,
+		snesPadLatchPin: -1,
+		snesPadDataPin: -1,
 		AnalogInputEnabled: 1,
 		BoardLedAddonEnabled: 1,
 		BuzzerSpeakerAddonEnabled: 1,
@@ -236,6 +239,7 @@ app.get("/api/getAddonsOptions", (req, res) => {
 		SliderSOCDInputEnabled: 1,
 		TurboInputEnabled: 1,
 		WiiExtensionAddonEnabled: 1,
+		SNESpadAddonEnabled: 1,
 		usedPins: Object.values(picoController),
 	});
 });
