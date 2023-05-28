c30b99c05aacfa9ce5675e5592f3a46e9af99f81
KazunoriNakayama <kanukanun@gmail.com>
2023-05-28 14:12:24 +0900

    Edit BoardConfig.h pin setting

--- configs/WaveshareZero/BoardConfig.h
+++ configs/WaveshareZero/BoardConfig.h
@@ -230,7 +230,7 @@
 #define TILT_SOCD_MODE SOCD_MODE_NEUTRAL
 
 // BOOTSEL Button Add-on setting
-#define BOOTSEL_BUTTON_MASK 12 // 0 means none, get other mask from GamepadState.h
+#define BOOTSEL_BUTTON_MASK 0 // 0 means none, get other mask from GamepadState.h
 
 // Extra Button Add-on setting
 #define EXTRA_BUTTON_MASK 0 // 0 means none, get other mask from GamepadState.h
