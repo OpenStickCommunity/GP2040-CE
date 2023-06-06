073990cb3b0806fb343a5e41cf53e2d7f1d3b875
ian <ian@vidales.se>
2023-06-06 20:35:59 +0200

    Add support for bootstrap-icons

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -16,6 +16,10 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+			"~bootstrap-icons": path.resolve(
+				__dirname,
+				"node_modules/bootstrap-icons"
+			),
 		},
 	},
 });
