a88f02a8106c42b3d5f04a165c941e2c3c8cced0
SavageCore <talk@savagecore.uk>
2023-07-09 03:27:02 +0100

    Remove more definitions, will it work?

--- configs/FlatboxRev5RGB/flatboxrev5rgb.h
+++ configs/FlatboxRev5RGB/flatboxrev5rgb.h
@@ -20,29 +20,4 @@
 #define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
 #endif
 
-// --- LED ---
-#ifndef PICO_DEFAULT_LED_PIN
-#define PICO_DEFAULT_LED_PIN 25
-#endif
-// no PICO_DEFAULT_WS2812_PIN
-
-// --- FLASH ---
-
-#define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
-
-#ifndef PICO_FLASH_SPI_CLKDIV
-#define PICO_FLASH_SPI_CLKDIV 2
-#endif
-
-#ifndef PICO_FLASH_SIZE_BYTES
-#define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
-#endif
-
-// Drive high to force power supply into PWM mode (lower ripple on 3V3 at light loads)
-#define PICO_SMPS_MODE_PIN 23
-
-#ifndef PICO_RP2040_B0_SUPPORTED
-#define PICO_RP2040_B0_SUPPORTED 1
-#endif
-
 #endif
