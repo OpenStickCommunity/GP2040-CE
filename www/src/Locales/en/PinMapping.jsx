0773fe59bd49116aa02b4164f562c3b3587fb16c
Rasmus Schultz <mindplay-dk@users.noreply.github.com>
2023-08-27 13:22:32 +0200

    Update PinMapping.jsx
    designate as "GPIO Pin Mapping" for complete clarity

--- www/src/Locales/en/PinMapping.jsx
+++ www/src/Locales/en/PinMapping.jsx
@@ -1,5 +1,5 @@
 export default {
-	'header-text': 'GPIO Mapping',
+	'header-text': 'GPIO Pin Mapping',
 	'sub-header-text':
 		'Use the form below to reconfigure your button-to-GPIO pin mapping.',
 	'alert-text':
