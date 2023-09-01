c2113f18bedf402fe8c0a1342f3131326a0d6bf2
ian <ian@vidales.se>
2023-09-01 19:22:02 +0200

    Extract wii addon

--- www/src/Pages/AddonsConfigPage.jsx
+++ www/src/Pages/AddonsConfigPage.jsx
@@ -47,6 +47,7 @@ import PSPassthrough, {
 	psPassthroughScheme,
 	psPassthroughState,
 } from '../Addons/Passthrough';
+import Wii, { wiiScheme, wiiState } from '../Addons/Wii';
 
 const schema = yup.object().shape({
 	FocusModeAddonEnabled: yup
@@ -87,29 +88,6 @@ const schema = yup.object().shape({
 		.label('Keyboard Host 5V Power Pin')
 		.validatePinWhenValue('KeyboardHostAddonEnabled'),
 
-	WiiExtensionAddonEnabled: yup
-		.number()
-		.required()
-		.label('Wii Extensions Enabled'),
-	wiiExtensionSDAPin: yup
-		.number()
-		.required()
-		.label('WiiExtension I2C SDA Pin')
-		.validatePinWhenValue('WiiExtensionAddonEnabled'),
-	wiiExtensionSCLPin: yup
-		.number()
-		.required()
-		.label('WiiExtension I2C SCL Pin')
-		.validatePinWhenValue('WiiExtensionAddonEnabled'),
-	wiiExtensionBlock: yup
-		.number()
-		.required()
-		.label('WiiExtension I2C Block')
-		.validateSelectionWhenValue('WiiExtensionAddonEnabled', I2C_BLOCKS),
-	wiiExtensionSpeed: yup
-		.number()
-		.label('WiiExtension I2C Speed')
-		.validateNumberWhenValue('WiiExtensionAddonEnabled'),
 	...analogScheme,
 	...bootselScheme,
 	...onBoardLedScheme,
@@ -125,13 +103,10 @@ const schema = yup.object().shape({
 	...socdScheme,
 	...ps4Scheme,
 	...psPassthroughScheme,
+	...wiiScheme,
 });
 
 const defaultValues = {
-	wiiExtensionSDAPin: -1,
-	wiiExtensionSCLPin: -1,
-	wiiExtensionBlock: 0,
-	wiiExtensionSpeed: 400000,
 	snesPadClockPin: -1,
 	snesPadLatchPin: -1,
 	snesPadDataPin: -1,
@@ -144,7 +119,6 @@ const defaultValues = {
 
 	KeyboardHostAddonEnabled: 0,
 
-	WiiExtensionAddonEnabled: 0,
 	SNESpadAddonEnabled: 0,
 	...analogState,
 	...bootselState,
@@ -161,6 +135,7 @@ const defaultValues = {
 	...socdState,
 	...ps4State,
 	...psPassthroughState,
+	...wiiState,
 };
 
 const ADDONS = [
@@ -179,6 +154,7 @@ const ADDONS = [
 	SOCD,
 	Ps4,
 	PSPassthrough,
+	Wii,
 ];
 
 const FormContext = ({ setStoredData }) => {
@@ -299,107 +275,6 @@ export default function AddonsConfigPage() {
 						/>
 					))}
 
-					<Section title={t('AddonsConfig:wii-extension-header-text')}>
-						<div
-							id="WiiExtensionAddonOptions"
-							hidden={!values.WiiExtensionAddonEnabled}
-						>
-							<Row>
-								<Trans
-									ns="AddonsConfig"
-									i18nKey="wii-extension-sub-header-text"
-								>
-									<p>
-										Note: If the Display is enabled at the same time, this Addon
-										will be disabled.
-									</p>
-									<h3>Currently Supported Controllers</h3>
-									<p>
-										Classic/Classic Pro: Both Analogs and D-Pad Supported. B =
-										B1, A = B2, Y = B3, X = B4, L = L1, ZL = L2, R = R1, ZR =
-										R2, Minus = S1, Plus = S2, Home = A1
-									</p>
-									<p>Nunchuck: Analog Stick Supported. C = B1, Z = B2</p>
-									<p>
-										Guitar Hero Guitar: Analog Stick Supported. Green = B1, Red
-										= B2, Blue = B3, Yellow = B4, Orange = L1, Strum Up = Up,
-										Strum Down = Down, Minus = S1, Plus = S2
-									</p>
-								</Trans>
-							</Row>
-							<Row className="mb-3">
-								<FormControl
-									type="number"
-									label={t('AddonsConfig:wii-extension-sda-pin-label')}
-									name="wiiExtensionSDAPin"
-									className="form-control-sm"
-									groupClassName="col-sm-3 mb-3"
-									value={values.wiiExtensionSDAPin}
-									error={errors.wiiExtensionSDAPin}
-									isInvalid={errors.wiiExtensionSDAPin}
-									onChange={handleChange}
-									min={-1}
-									max={29}
-								/>
-								<FormControl
-									type="number"
-									label={t('AddonsConfig:wii-extension-scl-pin-label')}
-									name="wiiExtensionSCLPin"
-									className="form-select-sm"
-									groupClassName="col-sm-3 mb-3"
-									value={values.wiiExtensionSCLPin}
-									error={errors.wiiExtensionSCLPin}
-									isInvalid={errors.wiiExtensionSCLPin}
-									onChange={handleChange}
-									min={-1}
-									max={29}
-								/>
-								<FormSelect
-									label={t('AddonsConfig:wii-extension-block-label')}
-									name="wiiExtensionBlock"
-									className="form-select-sm"
-									groupClassName="col-sm-3 mb-3"
-									value={values.wiiExtensionBlock}
-									error={errors.wiiExtensionBlock}
-									isInvalid={errors.wiiExtensionBlock}
-									onChange={handleChange}
-								>
-									{I2C_BLOCKS.map((o, i) => (
-										<option
-											key={`wiiExtensionI2cBlock-option-${i}`}
-											value={o.value}
-										>
-											{o.label}
-										</option>
-									))}
-								</FormSelect>
-								<FormControl
-									label={t('AddonsConfig:wii-extension-speed-label')}
-									name="wiiExtensionSpeed"
-									className="form-control-sm"
-									groupClassName="col-sm-3 mb-3"
-									value={values.wiiExtensionSpeed}
-									error={errors.wiiExtensionSpeed}
-									isInvalid={errors.wiiExtensionSpeed}
-									onChange={handleChange}
-									min={100000}
-								/>
-							</Row>
-						</div>
-						<FormCheck
-							label={t('Common:switch-enabled')}
-							type="switch"
-							id="WiiExtensionButton"
-							reverse={true}
-							error={undefined}
-							isInvalid={false}
-							checked={Boolean(values.WiiExtensionAddonEnabled)}
-							onChange={(e) => {
-								handleCheckbox('WiiExtensionAddonEnabled', values);
-								handleChange(e);
-							}}
-						/>
-					</Section>
 					<Section title={t('AddonsConfig:snes-extension-header-text')}>
 						<div id="SNESpadAddonOptions" hidden={!values.SNESpadAddonEnabled}>
 							<Row>
