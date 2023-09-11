873793d4e35c67d017d3a630529a4424f142ba13
KazunoriNakayama <kanukanun@gmail.com>
2023-09-11 16:56:06 +0900

    Reflected the changes from the merged pull request.

--- src/addons/tilt.cpp
+++ src/addons/tilt.cpp
@@ -156,8 +156,8 @@ void TiltInput::process()
 //Since this is an auxiliary function for appeals and such,
 //pressing Tilt1 and Tilt2 at the same time will cause the light analog stick to correspond to each of the DPad methods.
 void TiltInput::OverrideGamepad(Gamepad* gamepad, uint8_t dpad1, uint8_t dpad2) {
-	bool pinTilt1Pressed = pinTilt1 != (uint8_t)-1 && !gpio_get(pinTilt1);
-	bool pinTilt2Pressed = pinTilt2 != (uint8_t)-1 && !gpio_get(pinTilt2);
+	bool pinTilt1Pressed = (pinTilt1 != (uint8_t)-1) ? !gpio_get(pinTilt1) : false;
+	bool pinTilt2Pressed = (pinTilt2 != (uint8_t)-1) ? !gpio_get(pinTilt2) : false;
 	
 	double scaledTilt1FactorLeftX  = tilt1FactorLeftX  / 100.0;
 	double scaledTilt1FactorLeftY  = tilt1FactorLeftY  / 100.0;
