8ea273d75d36871c5c1aa05088c77058e4efe677
jack2game <rex.txt@gmail.com>
2023-10-19 23:29:55 +0800

    Update BoardConfig.h

--- configs/Pico/BoardConfig.h
+++ configs/Pico/BoardConfig.h
@@ -244,9 +244,6 @@
 #define TILT1_FACTOR_LEFT_Y  35  //Default value for the TILT button to function.
 #define TILT1_FACTOR_RIGHT_X 35  //Default value for the TILT button to function.
 #define TILT1_FACTOR_RIGHT_Y 35  //Default value for the TILT button to function.
-#define TILT1_FACTOR_LEFT_Y 35 //Default value for the TILT button to function.
-#define TILT1_FACTOR_RIGHT_X 35  //Default value for the TILT button to function.
-#define TILT1_FACTOR_RIGHT_Y 35 //Default value for the TILT button to function.
 #define PIN_TILT_2 -1
 #define TILT2_FACTOR_LEFT_X  65  //Default value for the TILT button to function.
 #define TILT2_FACTOR_LEFT_Y  65  //Default value for the TILT button to function.
@@ -262,12 +259,19 @@
 #define PIN_ANALOGMOD_RIGHT_ANALOG_RIGHT -1
 #define ANALOGMOD_SOCD_MODE SOCD_MODE_NEUTRAL
 #define PIN_ROTATE_1 -1
-#define ROTATE1_FACTOR_LEFT  15  //Default value for the ROTATE button to function.
-#define ROTATE1_FACTOR_RIGHT 15  //Default value for the ROTATE button to function.
 #define PIN_ROTATE_2 -1
-#define ROTATE2_FACTOR_LEFT  345  //Default value for the ROTATE button to function.
-#define ROTATE2_FACTOR_RIGHT 345  //Default value for the ROTATE button to function.
-#define PIN_TILT_FUNCTION -1
+#define ROTATE1_FACTOR_LEFT  15  //Default value for the ROTATE button to function.
+#define ROTATE2_FACTOR_LEFT  345 //Default value for the ROTATE button to function.
+#define ROTATE3_FACTOR_LEFT  0   //Default value for the ROTATE button to function.
+#define ROTATE4_FACTOR_LEFT  0   //Default value for the ROTATE button to function.
+#define ROTATE5_FACTOR_LEFT  0   //Default value for the ROTATE button to function.
+#define ROTATE6_FACTOR_LEFT  0   //Default value for the ROTATE button to function.
+#define ROTATE1_FACTOR_RIGHT 15  //Default value for the ROTATE button to function.
+#define ROTATE2_FACTOR_RIGHT 345 //Default value for the ROTATE button to function.
+#define ROTATE3_FACTOR_RIGHT 0   //Default value for the ROTATE button to function.
+#define ROTATE4_FACTOR_RIGHT 0   //Default value for the ROTATE button to function.
+#define ROTATE5_FACTOR_RIGHT 0   //Default value for the ROTATE button to function.
+#define ROTATE6_FACTOR_RIGHT 0   //Default value for the ROTATE button to function.
 #define PIN_TILT_LEFT_ANALOG_UP -1
 #define PIN_TILT_LEFT_ANALOG_DOWN -1
 #define PIN_TILT_LEFT_ANALOG_LEFT -1
@@ -276,7 +280,8 @@
 #define PIN_TILT_RIGHT_ANALOG_DOWN -1
 #define PIN_TILT_RIGHT_ANALOG_LEFT -1
 #define PIN_TILT_RIGHT_ANALOG_RIGHT -1
-#define TILT_SOCD_MODE SOCD_MODE_NEUTRAL
+#define ANALOGMOD_LEFT_SOCD_MODE SOCD_MODE_NEUTRAL
+#define ANALOGMOD_RIGHT_SOCD_MODE SOCD_MODE_NEUTRAL
 
 // BOOTSEL Button Add-on setting
 #define BOOTSEL_BUTTON_MASK 0 // 0 means none, get other mask from GamepadState.h
