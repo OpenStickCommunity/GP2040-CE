c26889f7084d3dcdedfe1e7049dba2265726f175
jack2game <rex.txt@gmail.com>
2023-10-20 08:44:52 +0800

    Merge branch 'OpenStickCommunity:main' into Rotation (#7)
    * Fixes Macro type Press and Loop issue (#587)
    
    * Fix looping macro first input line duration error
    
    * Fix Press macro interruptible check with debounce
    
    * Remove onboard LED pin definition from devices without an onboard LED (#590)
    
    ---------
    
    Co-authored-by: deeebug <77402236+deeebug@users.noreply.github.com>
    Co-authored-by: TheTrain <32771064+TheTrainGoes@users.noreply.github.com>

--- src/addons/input_macro.cpp
+++ src/addons/input_macro.cpp
@@ -38,6 +38,8 @@ void InputMacro::preprocess()
     if (focusModeOptions.enabled && focusModeOptions.macroLockEnabled) return;
     Gamepad * gamepad = Storage::getInstance().GetGamepad();
 	uint32_t allPins = ~gpio_get_all();
+    macroInputPressed = false;
+    uint64_t currentMicros = getMicro();
 
     if (macroPosition == -1 || inputMacroOptions.macroList[macroPosition].interruptible) {
         int newMacroPosition = -1;
@@ -69,10 +71,21 @@ void InputMacro::preprocess()
             if (newMacroPosition != macroPosition ||
                 (newMacroPosition == macroPosition &&
                  inputMacroOptions.macroList[newMacroPosition].macroType == ON_PRESS &&
-                 isMacroRunning)) {
-                    reset();
-                    return;
-                 }
+                 isMacroRunning && (macroTriggerDebounceStartTime != 0 || (!prevMacroInputPressed && macroInputPressed)))) {
+                    if (macroTriggerDebounceStartTime == 0) {
+                        macroTriggerDebounceStartTime = currentMicros;
+                    }
+
+                    if (macroTriggerDebounceStartTime != 0) {
+                        if (((currentMicros - macroTriggerDebounceStartTime) > 500)) {
+                            macroTriggerDebounceStartTime = 0;
+                            if (macroInputPressed) {
+                                reset();
+                                return;
+                            }
+                        }
+                    }
+            }
         }
 
         if (newMacroPosition != -1 && !isMacroRunning) {
@@ -90,8 +103,6 @@ void InputMacro::preprocess()
     } else {
         macroInputPressed = (allPins & 1 << macro.macroTriggerPin);
     }
-
-    uint64_t currentMicros = getMicro();
 
     if (!isMacroRunning && macroInputPressed && macroTriggerDebounceStartTime == 0) {
         macroTriggerDebounceStartTime = currentMicros;
@@ -138,11 +149,11 @@ void InputMacro::preprocess()
 
     MacroInput& macroInput = macro.macroInputs[macroInputPosition];
     uint32_t macroInputDuration = macroInput.duration + macroInput.waitDuration;
+    macroInputHoldTime = macroInputDuration <= 0 ? INPUT_HOLD_US : macroInputDuration;
 
     if (!isMacroRunning && isMacroTriggerHeld) {
         isMacroRunning = true;
         macroStartTime = currentMicros;
-        macroInputHoldTime = macroInputDuration <= 0 ? INPUT_HOLD_US : macroInputDuration;
     }
     
     if (!isMacroRunning)
@@ -205,10 +216,7 @@ void InputMacro::preprocess()
         isMacroTriggerHeld = isMacroTriggerHeld && isMacroTypeLoopable;
         isMacroRunning = isMacroTriggerHeld;
         macroPosition = (isMacroTypeLoopable && isMacroTriggerHeld) ? macroPosition : -1;
-        if (isMacroTypeLoopable && !isMacroTriggerHeld) {
-            macroStartTime = 0;
-            macroInputHoldTime = INPUT_HOLD_US;
-        }
+        macroStartTime = currentMicros;
     }
 }
 
