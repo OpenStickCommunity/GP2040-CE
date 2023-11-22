de96f8486209da457fa0ad9c37407066f9dc9e0f
ian <ian@vidales.se>
2023-11-22 01:35:17 +0100

    Cleanup incorrect props in app, remove profile settings page

--- www/src/Locales/pt-BR/PinMapping.jsx
+++ www/src/Locales/pt-BR/PinMapping.jsx
@@ -5,6 +5,8 @@ export default {
 	'alert-text':
 		'Associar botões a pinos que não estão conectados ou disponíveis pode deixar o dispositivo em um estado não funcional. Para limpar a configuração inválida, vá para a página <1>Redefinir Configurações</1>.',
 	'pin-header-label': 'Pino',
+	'profile-pins-warning':
+		'Tente evitar alterar os botões/direções usados para seus atalhos de perfil de troca, caso contrário, ficará difícil entender qual perfil você está selecionando!',
 	errors: {
 		conflict: 'O Pino {{pin}} já está atribuído a {{conflictedMappings}}',
 		required: '{{button}} é obrigatório',
