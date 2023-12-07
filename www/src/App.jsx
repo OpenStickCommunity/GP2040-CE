274edbd6b8f70ef04231df913a1c235ea3529079
ian <ian@vidales.se>
2023-12-07 18:35:57 +0100

    Remove unused imports

--- www/src/App.jsx
+++ www/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { AppContextProvider } from './Contexts/AppContext';
