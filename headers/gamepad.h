5e8761c791a7fbdda43a443541699755c04bf64e
Luke Arntson <arntsonl@gmail.com>
2023-12-27 16:28:10 -0500

    v0.7.6-RC3 FIX: Xbox One reporting issues (#727)
    * Clean-up and two fixes: sequence no longer goes to 0 and keep alive does not trigger a repeat input report.
    
    * Removing AlpacaOwO in this branch as this is a desired change
    
    * This one is an extern so we can't static it

--- headers/gamepad.h
+++ headers/gamepad.h
@@ -91,6 +91,8 @@ public:
     XboxOriginalReport *getXboxOriginalReport();
 
 	uint8_t last_report[CFG_TUD_ENDPOINT0_SIZE] = { };
+	uint8_t last_report_counter = 0;
+	uint16_t last_axis_counter = 0;
 
 	/**
 	 * @brief Check for a button press. Used by `pressed[Button]` helper methods.
