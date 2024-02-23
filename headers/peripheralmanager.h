f00b1498344f85580908e98bc495c9d100484f93
Arkwolf <50710252+MajorArkwolf@users.noreply.github.com>
2024-02-23 20:41:12 +0800

    Update peripheralmanager.h

--- headers/peripheralmanager.h
+++ headers/peripheralmanager.h
@@ -9,9 +9,8 @@
 
 class PeripheralManager {
 public:
-    PeripheralManager(){}
 	PeripheralManager(PeripheralManager const&) = delete;
-	void operator=(PeripheralManager const&)  = delete;
+	PeripheralManager& operator=(PeripheralManager const&)  = delete;
 	static PeripheralManager& getInstance() // Thread-safe storage ensures cross-thread talk
 	{
 		static PeripheralManager instance;
@@ -30,6 +29,9 @@ public:
     bool isSPIEnabled(uint8_t block);
     bool isUSBEnabled(uint8_t block);
 private:
+		PeripheralManager() = default;
+    ~PeripheralManager() = default;
+
     PeripheralI2C blockI2C0;
     PeripheralI2C blockI2C1;
 
