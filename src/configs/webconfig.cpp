3bad42c57b56f2a0809f26f9c24aeb114b52ee07
tanakataroumaru1 <toppppy@song.ocn.ne.jp>
2024-03-24 08:46:56 +0900

    Analog Sensitivity Setting

--- src/configs/webconfig.cpp
+++ src/configs/webconfig.cpp
@@ -1248,6 +1248,7 @@ std::string setAddonOptions()
     docToValue(analogOptions.analog_deadzone, doc, "analog_deadzone");
     docToValue(analogOptions.auto_calibrate, doc, "auto_calibrate");
     docToValue(analogOptions.enabled, doc, "AnalogInputEnabled");
+	docToValue(analogOptions.analog_sensitivity, doc, "analog_sensitivity");
 
     BootselButtonOptions& bootselButtonOptions = Storage::getInstance().getAddonOptions().bootselButtonOptions;
     docToValue(bootselButtonOptions.buttonMap, doc, "bootselButtonMap");
@@ -1659,6 +1660,7 @@ std::string getAddonOptions()
     writeDoc(doc, "analog_deadzone", analogOptions.analog_deadzone);
     writeDoc(doc, "auto_calibrate", analogOptions.auto_calibrate);
     writeDoc(doc, "AnalogInputEnabled", analogOptions.enabled);
+   	writeDoc(doc, "analog_sensitivity", analogOptions.analog_sensitivity);
 
     const BootselButtonOptions& bootselButtonOptions = Storage::getInstance().getAddonOptions().bootselButtonOptions;
     writeDoc(doc, "bootselButtonMap", bootselButtonOptions.buttonMap);
