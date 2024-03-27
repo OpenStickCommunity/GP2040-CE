b99299e0fd5f689995172f2e6d8336918306b3e0
Luke Arntson <arntsonl@gmail.com>
2024-03-27 18:15:47 -0400

    Quick clean-up to move available and setup() pin settings

--- src/addons/input_macro.cpp
+++ src/addons/input_macro.cpp
@@ -12,31 +12,12 @@ bool InputMacro::available() {
     {
         switch( pinMappings[pin] ) {
             case GpioAction::BUTTON_PRESS_MACRO:
-                macroButtonMask = 1 << pin;
-                hasMacroAssigned = true;
-                break;
             case GpioAction::BUTTON_PRESS_MACRO_1:
-                macroPinMasks[0] = 1 << pin;
-                hasMacroAssigned = true;
-                break;
             case GpioAction::BUTTON_PRESS_MACRO_2:
-                macroPinMasks[1] = 1 << pin;
-                hasMacroAssigned = true;
-                break;
             case GpioAction::BUTTON_PRESS_MACRO_3:
-                macroPinMasks[2] = 1 << pin;
-                hasMacroAssigned = true;
-                break;
             case GpioAction::BUTTON_PRESS_MACRO_4:
-                macroPinMasks[3] = 1 << pin;
-                hasMacroAssigned = true;
-                break;
             case GpioAction::BUTTON_PRESS_MACRO_5:
-                macroPinMasks[4] = 1 << pin;
-                hasMacroAssigned = true;
-                break;
             case GpioAction::BUTTON_PRESS_MACRO_6:
-                macroPinMasks[5] = 1 << pin;
                 hasMacroAssigned = true;
                 break;
             default:
@@ -47,6 +28,36 @@ bool InputMacro::available() {
 }
 
 void InputMacro::setup() {
+    GpioAction* pinMappings = Storage::getInstance().getProfilePinMappings();
+    for (Pin_t pin = 0; pin < (Pin_t)NUM_BANK0_GPIOS; pin++)
+    {
+        switch( pinMappings[pin] ) {
+            case GpioAction::BUTTON_PRESS_MACRO:
+                macroButtonMask = 1 << pin;
+                break;
+            case GpioAction::BUTTON_PRESS_MACRO_1:
+                macroPinMasks[0] = 1 << pin;
+                break;
+            case GpioAction::BUTTON_PRESS_MACRO_2:
+                macroPinMasks[1] = 1 << pin;
+                break;
+            case GpioAction::BUTTON_PRESS_MACRO_3:
+                macroPinMasks[2] = 1 << pin;
+                break;
+            case GpioAction::BUTTON_PRESS_MACRO_4:
+                macroPinMasks[3] = 1 << pin;
+                break;
+            case GpioAction::BUTTON_PRESS_MACRO_5:
+                macroPinMasks[4] = 1 << pin;
+                break;
+            case GpioAction::BUTTON_PRESS_MACRO_6:
+                macroPinMasks[5] = 1 << pin;
+                break;
+            default:
+                break;
+        }
+    }
+
     inputMacroOptions = Storage::getInstance().getAddonOptions().macroOptions;
     if (inputMacroOptions.macroBoardLedEnabled && isValidPin(BOARD_LED_PIN)) {
         gpio_init(BOARD_LED_PIN);
