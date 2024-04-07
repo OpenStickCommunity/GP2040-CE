d9988c9ace2350ff3bab3ac650dcd5c4630b5bc2
Mike Parks <mikepparks@gmail.com>
2024-04-06 20:05:02 -0700

    Returned ranges for Les Paul back to expected values.

--- lib/WiiExtension/extensions/GuitarExtension.cpp
+++ lib/WiiExtension/extensions/GuitarExtension.cpp
@@ -69,10 +69,10 @@ void GuitarExtension::init(uint8_t dataType) {
     _analogCalibration[WiiAnalogs::WII_ANALOG_LEFT_Y].center           = WII_GUITAR_GATE_CENTER;
     _analogCalibration[WiiAnalogs::WII_ANALOG_LEFT_Y].maximum          = WII_GUITAR_GATE_CENTER+WII_GUITAR_GATE_SIZE;
     
-    _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].minimum         = 241;
-    _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].center          = 243;
-    _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].maximum         = 246;
-    _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].useOffset       = false;
+    _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].minimum         = 131;
+    _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].center          = 169;
+    _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].maximum         = 207;
+    _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].useOffset       = true;
 
     _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_Y].minimum         = 0;
     _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_Y].center          = WII_GUITAR_GATE_CENTER;
@@ -102,7 +102,7 @@ void GuitarExtension::process(uint8_t *inputData) {
             _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].minimum         = 15;
             _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].center          = 19;
             _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].maximum         = 20;
-            _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].useOffset       = false;
+            _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_X].useOffset       = true;
 
             _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_Y].minimum         = 0;
             _analogCalibration[WiiAnalogs::WII_ANALOG_RIGHT_Y].center          = WII_GUITAR_GATE_CENTER;
