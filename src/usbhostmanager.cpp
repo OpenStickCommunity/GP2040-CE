2300ffa98ba26f60570cf068c2ce4622c557c328
Brian S. Stephan <bss@incorporeal.org>
2024-06-10 16:05:16 -0500

    don't initialize the USB host if no one is listening
    found this while looking into a different issue. if there's no
    drivers/addons that have registered as a USB host listener, there's no
    point in initializing it, so don't

--- src/usbhostmanager.cpp
+++ src/usbhostmanager.cpp
@@ -12,7 +12,7 @@
 
 void USBHostManager::start() {
     // This will happen after Gamepad has initialized
-    if (PeripheralManager::getInstance().isUSBEnabled(0)) {
+    if (PeripheralManager::getInstance().isUSBEnabled(0) && listeners.size() > 0) {
         pio_usb_configuration_t* pio_cfg = PeripheralManager::getInstance().getUSB(0)->getController();
         tuh_configure(1, TUH_CFGID_RPI_PIO_USB_CONFIGURATION, pio_cfg);
         tuh_init(BOARD_TUH_RHPORT);
