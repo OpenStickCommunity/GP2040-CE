32bafb2645b390b33ced9c235e3bbae281582004
Nicolas Merz <nicolasmerz19@gmail.com>
2024-07-04 10:49:30 -0400

    Create hard-coded DRV8833RumbleAddon, test 0 and 255 values

---
+++ headers/addons/drv8833_rumble.h
@@ -0,0 +1,62 @@
+#ifndef RUMBLE_H_
+#define RUMBLE_H_
+
+#include <vector>
+#include <string>
+#include "gpaddon.h"
+#include "gamepad/GamepadState.h"
+
+#ifndef RUMBLE_ENABLED
+#define RUMBLE_ENABLED 1
+#endif
+
+#ifndef LEFT_MOTOR_PIN
+#define LEFT_MOTOR_PIN 26
+#endif
+
+#ifndef RIGHT_MOTOR_PIN
+#define RIGHT_MOTOR_PIN 27
+#endif
+
+#ifndef MOTOR_SLEEP_PIN
+#define MOTOR_SLEEP_PIN 22
+#endif
+
+#ifndef PWM_FREQUENCY
+#define PWM_FREQUENCY 10000 // 10 kHz
+#endif
+
+// DRV8833 Rumble Module
+#define DRV8833RumbleName "DRV8833Rumble"
+
+// Scale uint8 to 0 - 100 range
+#define motorToDuty(m) (100.0f * (m/255.0f))
+
+// Buzzer Speaker
+class DRV8833RumbleAddon : public GPAddon
+{
+public:
+	virtual bool available();
+	virtual void setup();
+	virtual void preprocess() {}
+	virtual void process();
+	virtual std::string name() { return DRV8833RumbleName; }
+private:
+	uint32_t pwmSetFreqDuty(uint slice, uint channel, uint32_t frequency, float duty);
+	bool compareRumbleState(Gamepad * gamepad);
+	void setRumbleState(Gamepad * gamepad);
+	void disableMotors();
+	void enableMotors(Gamepad * gamepad);
+	uint8_t leftMotorPin;
+	uint8_t rightMotorPin;
+	uint8_t motorSleepPin;
+	uint8_t leftMotorPinSlice;
+	uint8_t leftMotorPinChannel;
+	uint8_t rightMotorPinSlice;
+	uint8_t rightMotorPinChannel;
+	uint32_t pwmFrequency;
+	uint32_t sysClock;
+	GamepadRumbleState currentRumbleState;
+};
+
+#endif
