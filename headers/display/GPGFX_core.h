10826898df25444667b09b7510a26611b3408046
Mike Parks <mikepparks@gmail.com>
2024-08-11 19:20:54 -0700

    Added "Pin Viewer" and "Stats" screen types to Display. Moved special character definitions to global Display defines. Included new "Build", "Config", and "Platform" conditionals. Fixed issue where text rendering would wrap if line was too long. To-Do: add wrapping abilities to tiny_ssd1306.

--- headers/display/GPGFX_core.h
+++ headers/display/GPGFX_core.h
@@ -8,4 +8,31 @@
 #include "fonts/GP_Font_Big.h"
 #include "fonts/GP_Font_Standard.h"
 
+// special characters
+#define CHAR_TRIANGLE "\x80"
+#define CHAR_CIRCLE   "\x81"
+#define CHAR_CROSS    "\x82"
+#define CHAR_SQUARE   "\x83"
+
+#define CHAR_UP       "\x84"
+#define CHAR_DOWN     "\x85"
+#define CHAR_LEFT     "\x86"
+#define CHAR_RIGHT    "\x87"
+
+#define CHAR_UL       "\x88"
+#define CHAR_UR       "\x89"
+#define CHAR_DL       "\x8A"
+#define CHAR_DR       "\x8B"
+
+#define CHAR_CAP_S    "\x8C"
+#define CHAR_HOME_S   "\x8D"
+
+#define CHAR_VIEW_X   "\x8E"
+#define CHAR_MENU_X   "\x8F"
+#define CHAR_HOME_X   "\x90"
+
+#define CHAR_TPAD_P   "\x91"
+#define CHAR_HOME_P   "\x92"
+#define CHAR_SHARE_P  "\x93"
+
 #endif
