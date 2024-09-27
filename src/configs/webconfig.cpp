d327d4095bd1550e8f0cef80f85cafaa8b882a6e
Brian S. Stephan <bss@incorporeal.org>
2024-09-27 16:16:35 -0500

    we need to update the pin count in protobuf for it to save properly

--- src/configs/webconfig.cpp
+++ src/configs/webconfig.cpp
@@ -560,6 +560,8 @@ std::string setProfileOptions()
                 profileOptions.gpioMappingsSets[altsIndex].pins[pin].customDpadMask = (uint32_t)alt[pinName]["customDpadMask"];
             }
         }
+        profileOptions.gpioMappingsSets[altsIndex].pins_count = NUM_BANK0_GPIOS;
+
         size_t profileLabelSize = sizeof(profileOptions.gpioMappingsSets[altsIndex].profileLabel);
         strncpy(profileOptions.gpioMappingsSets[altsIndex].profileLabel, alt["profileLabel"], profileLabelSize - 1);
         profileOptions.gpioMappingsSets[altsIndex].profileLabel[profileLabelSize - 1] = '\0';
