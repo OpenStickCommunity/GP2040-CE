b2c49aedb2855dfe21c161f08eaac188cbb27d67
mizma <omoikane@path-works.net>
2024-09-28 23:23:17 +0900

    add missing Japanese Localization strings

---
+++ www/src/Locales/ja-JP/Addons/ReactiveLED.jsx
@@ -0,0 +1,14 @@
+export default {
+	'header-text': 'リアクティブ LED',
+	'button-save': 'LED 保存',
+	'led-pin-label': 'LED 端子',
+	'led-action-label': 'アクション',
+	'led-mode-up-label': 'デフォルトモード',
+	'led-mode-down-label': 'アクティブモード',
+	'led-mode': {
+		'static-on': 'ON固定',
+		'static-off': 'OFF固定',
+		'fade-in': 'フェードイン',
+		'fade-out': 'フェードアウト',
+	},
+};
