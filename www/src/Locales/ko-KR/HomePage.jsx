55bdd2eb22dda033e97cd820ea7603b0f3ecec8e
Tomuri32 <86832197+Tomuri32@users.noreply.github.com>
2024-09-30 00:07:14 +0900

    Create HomePage.jsx

---
+++ www/src/Locales/ko-KR/HomePage.jsx
@@ -0,0 +1,14 @@
+export default {
+	'current-text': '버전: {{version}}',
+	'get-update-text': '최신 버전을 다운로드',
+	'header-text': 'GP2040-CE Web Configurator 에 오신걸 환영합니다.',
+	'latest-text': '최신버전: {{version}}',
+	'memory-flash-text': '플래쉬',
+	'memory-board-text': '기판 플래시',
+	'memory-header-text': '메모리(KB)',
+	'memory-heap-text': '힙',
+	'memory-static-allocations-text': '할당중인 메모리',
+	'sub-header-text': '메뉴에서 사용할 옵션을 선택하세요。',
+	'system-stats-header-text': '시스템 정보',
+	'version-text': '버전',
+};
