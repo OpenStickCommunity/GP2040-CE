e8c8f0e1b8f7435bde80ab8740719c1b280c167c
Tomuri32 <86832197+Tomuri32@users.noreply.github.com>
2024-09-30 00:48:30 +0900

    Create ResetSettings.jsx

---
+++ www/src/Locales/ko-KR/ResetSettings.jsx
@@ -0,0 +1,6 @@
+export default {
+	'header-text': '설정 초기화',
+	'sub-header-text':
+		'<0 className="card-text"> 이 옵션은 컨트롤러에 저장되어 있던 모든 설정을 초기화합니다.</0> <0 className="card-text">이 처리는 자동으로 컨트롤러를 재기동 합니다.</0>',
+	'confirm-text': '정말로 설정을 초기화 하시겠습니까?',
+};
