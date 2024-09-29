b081a32a58c63df7a782f9411e6c1c5f0fc994a9
Tomuri32 <86832197+Tomuri32@users.noreply.github.com>
2024-09-29 23:44:04 +0900

    ko-KR lang add

---
+++ www/src/Locales/ko-KR/CaptureButton.jsx
@@ -0,0 +1,7 @@
+export default {
+	'capture-button-button-label': '컨트롤러 단자를 할당',
+	'capture-button-modal-title': '버튼 입력 대기：',
+	'capture-button-modal-content': '컨트롤러의 버튼을 눌러주세요.',
+	'capture-button-modal-skip': '버튼 스킵',
+	'capture-button-modal-stop': '설정 정지',
+};
