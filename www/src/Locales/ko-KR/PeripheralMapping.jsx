506bf2e61a909bf6fd381cc80cacd92e6bed1509
Tomuri32 <86832197+Tomuri32@users.noreply.github.com>
2024-09-30 00:38:16 +0900

    Create PeripheralMapping.jsx

---
+++ www/src/Locales/ko-KR/PeripheralMapping.jsx
@@ -0,0 +1,29 @@
+export default {
+	'header-text': '주변 기기 설정',
+	'sub-header-text':
+		'여기에서는 주변기기의 단자 할당 또는 동작에 관련된 설정을 할 수 있습니다',
+	'pins-label': '단자',
+	'pin-in-use': '할당완료',
+	'i2c-desc-header': 'I\u00B2C 정보',
+	'i2c-description':
+		'I\u00B2C는 주변 장치 연결에 사용되는 SDA 라인과 SCL 라인을 이용한 2선식 통신 인터페이스로, 디스플레이, GPIO 확장, 센서 등에 사용됩니다. RP2040에서는 두 개의I\u00B2C블록을 지원합니다(I2C0과 I2C1). 아래의 핀들이 각 블록에서 사용할 수 있는 GPIO 핀입니다.',
+	'spi-desc-header': 'SPI 정도',
+	'spi-description':
+		'SPI는 주변 장치 연결에 사용되는 RX, TX, 데이터 라인과 SCK 클럭 라인으로 구성된 4선식 통신 인터페이스로, 디스플레이, GPIO 확장, 센서 등에 사용됩니다. RP2040에서는 두 개의 SPI 컨트롤러 블록을 지원합니다(SPI0와 SPI1). 아래의 핀들이 각 블록에서 사용할 수 있는 GPIO 핀입니다',
+	'usb-label': 'USB호스트',
+	'usb-desc-header': 'USB호스트 정보',
+	'usb-description':
+		'USB 호스트 기능은 USB 클라이언트의 연결에 사용되며, 키보드, 게임 컨트롤러 등의 장치를 연결할 수 있습니다',
+	'peripheral-toggle-unavailable':
+		'활성화 할 수 없습니다. 주변기기 {{name}} 는 이용 할 수 없습니다.<0>주변기기설정</0>에서 활성화 해 주세요.',
+	'pin-sda-label': 'SDA단자',
+	'pin-scl-label': 'SCL단자',
+	'option-speed-label': '속도',
+	'pin-rx-label': 'RX단자',
+	'pin-cs-label': 'CS단자',
+	'pin-sck-label': 'SCK단자',
+	'pin-tx-label': 'TX단자',
+	'pin-dp-label': 'D+단자',
+	'pin-enable5v-label': '5V활성화',
+	'option-order-label': '단자순서',
+};
