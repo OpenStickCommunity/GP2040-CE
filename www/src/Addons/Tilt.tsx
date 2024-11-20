d684de5b4443825c2b16f3a80005a937efd1b26a
Mike Parks <mikepparks@gmail.com>
2024-11-19 21:23:52 -0800

    Removed deprecated pins from webconfig Addon views

--- www/src/Addons/Tilt.tsx
+++ www/src/Addons/Tilt.tsx
@@ -11,10 +11,6 @@ import { SOCD_MODES, TILT_SOCD_MODES } from '../Data/Addons';
 
 export const tiltScheme = {
 	TiltInputEnabled: yup.number().required().label('Tilt Input Enabled'),
-	tilt1Pin: yup
-		.number()
-		.label('Tilt 1 Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
 	factorTilt1LeftX: yup
 		.number()
 		.label('Tilt 1 Factor Left X')
@@ -31,10 +27,6 @@ export const tiltScheme = {
 		.number()
 		.label('Tilt 1 Factor Right Y')
 		.validateRangeWhenValue('TiltInputEnabled', 0, 100),
-	tilt2Pin: yup
-		.number()
-		.label('Tilt 2 Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
 	factorTilt2LeftX: yup
 		.number()
 		.label('Tilt 2 Factor Left X')
@@ -51,38 +43,6 @@ export const tiltScheme = {
 		.number()
 		.label('Tilt 2 Factor Right Y')
 		.validateRangeWhenValue('TiltInputEnabled', 0, 100),
-	tiltLeftAnalogUpPin: yup
-		.number()
-		.label('Tilt Left Analog Up Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
-	tiltLeftAnalogDownPin: yup
-		.number()
-		.label('Tilt Left Analog Down Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
-	tiltLeftAnalogLeftPin: yup
-		.number()
-		.label('Tilt Left Analog Left Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
-	tiltLeftAnalogRightPin: yup
-		.number()
-		.label('Tilt Left Analog Right Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
-	tiltRightAnalogUpPin: yup
-		.number()
-		.label('Tilt Right Analog Up Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
-	tiltRightAnalogDownPin: yup
-		.number()
-		.label('Tilt Right Analog Down Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
-	tiltRightAnalogLeftPin: yup
-		.number()
-		.label('Tilt Right Analog Left Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
-	tiltRightAnalogRightPin: yup
-		.number()
-		.label('Tilt Right Analog Right Pin')
-		.validatePinWhenValue('TiltInputEnabled'),
 	tiltSOCDMode: yup
 		.number()
 		.label('Tilt SOCE Mode')
@@ -91,24 +51,14 @@ export const tiltScheme = {
 
 export const tiltState = {
 	TiltInputEnabled: 0,
-	tilt1Pin: -1,
 	factorTilt1LeftX: 0,
 	factorTilt1LeftY: 0,
 	factorTilt1RightX: 0,
 	factorTilt1ightY: 0,
-	tilt2Pin: -1,
 	factorTilt2LeftX: 0,
 	factorTilt2LeftY: 0,
 	factorTilt2RightX: 0,
 	factorTilt2RightY: 0,
-	tiltLeftAnalogUpPin: -1,
-	tiltLeftAnalogDownPin: -1,
-	tiltLeftAnalogLeftPin: -1,
-	tiltLeftAnalogRightPin: -1,
-	tiltRightAnalogUpPin: -1,
-	tiltRightAnalogDownPin: -1,
-	tiltRightAnalogLeftPin: -1,
-	tiltRightAnalogRightPin: -1,
 };
 
 const Tilt = ({ values, errors, handleChange, handleCheckbox }) => {
@@ -116,21 +66,6 @@ const Tilt = ({ values, errors, handleChange, handleCheckbox }) => {
 	return (
 		<Section title={t('AddonsConfig:tilt-header-text')}>
 			<div id="TiltOptions" hidden={!values.TiltInputEnabled}>
-				<Row className="mb-3">
-					<FormControl
-						type="number"
-						label={t('AddonsConfig:tilt-1-pin-label')}
-						name="tilt1Pin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tilt1Pin}
-						error={errors.tilt1Pin}
-						isInvalid={errors.tilt1Pin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
-					/>
-				</Row>
 				<Row className="mb-3">
 					<FormControl
 						type="number"
@@ -188,21 +123,6 @@ const Tilt = ({ values, errors, handleChange, handleCheckbox }) => {
 				<Row className="mb-3">
 					<FormControl
 						type="number"
-						label={t('AddonsConfig:tilt-2-pin-label')}
-						name="tilt2Pin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tilt2Pin}
-						error={errors.tilt2Pin}
-						isInvalid={errors.tilt2Pin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
-					/>
-				</Row>
-				<Row className="mb-3">
-					<FormControl
-						type="number"
 						label={t('AddonsConfig:tilt-2-factor-left-x-label')}
 						name="factorTilt2LeftX"
 						className="form-select-sm"
@@ -252,114 +172,6 @@ const Tilt = ({ values, errors, handleChange, handleCheckbox }) => {
 						onChange={handleChange}
 						min={-1}
 						max={200}
-					/>
-				</Row>
-				<Row className="mb-3">
-					<FormControl
-						type="number"
-						label={t('AddonsConfig:tilt-left-analog-up-pin-label')}
-						name="tiltLeftAnalogUpPin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tiltLeftAnalogUpPin}
-						error={errors.tiltLeftAnalogUpPin}
-						isInvalid={errors.tiltLeftAnalogUpPin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
-					/>
-					<FormControl
-						type="number"
-						label={t('AddonsConfig:tilt-left-analog-down-pin-label')}
-						name="tiltLeftAnalogDownPin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tiltLeftAnalogDownPin}
-						error={errors.tiltLeftAnalogDownPin}
-						isInvalid={errors.tiltLeftAnalogDownPin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
-					/>
-					<FormControl
-						type="number"
-						label={t('AddonsConfig:tilt-left-analog-left-pin-label')}
-						name="tiltLeftAnalogLeftPin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tiltLeftAnalogLeftPin}
-						error={errors.tiltLeftAnalogLeftPin}
-						isInvalid={errors.tiltLeftAnalogLeftPin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
-					/>
-					<FormControl
-						type="number"
-						label={t('AddonsConfig:tilt-left-analog-right-pin-label')}
-						name="tiltLeftAnalogRightPin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tiltLeftAnalogRightPin}
-						error={errors.tiltLeftAnalogRightPin}
-						isInvalid={errors.tiltLeftAnalogRightPin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
-					/>
-				</Row>
-				<Row className="mb-3">
-					<FormControl
-						type="number"
-						label={t('AddonsConfig:tilt-right-analog-up-pin-label')}
-						name="tiltRightAnalogUpPin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tiltRightAnalogUpPin}
-						error={errors.tiltRightAnalogUpPin}
-						isInvalid={errors.tiltRightAnalogUpPin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
-					/>
-					<FormControl
-						type="number"
-						label={t('AddonsConfig:tilt-right-analog-down-pin-label')}
-						name="tiltRightAnalogDownPin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tiltRightAnalogDownPin}
-						error={errors.tiltRightAnalogDownPin}
-						isInvalid={errors.tiltRightAnalogDownPin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
-					/>
-					<FormControl
-						type="number"
-						label={t('AddonsConfig:tilt-right-analog-left-pin-label')}
-						name="tiltRightAnalogLeftPin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tiltRightAnalogLeftPin}
-						error={errors.tiltRightAnalogLeftPin}
-						isInvalid={errors.tiltRightAnalogLeftPin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
-					/>
-					<FormControl
-						type="number"
-						label={t('AddonsConfig:tilt-right-analog-right-pin-label')}
-						name="tiltRightAnalogRightPin"
-						className="form-select-sm"
-						groupClassName="col-sm-3 mb-3"
-						value={values.tiltRightAnalogRightPin}
-						error={errors.tiltRightAnalogRightPin}
-						isInvalid={errors.tiltRightAnalogRightPin}
-						onChange={handleChange}
-						min={-1}
-						max={29}
 					/>
 				</Row>
 				<Row className="mb-3">
