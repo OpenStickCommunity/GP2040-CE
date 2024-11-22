41d1443982812cea0f9b20d3200b55a9b0b48e73
shinzero96 <136745149+shinzero96@users.noreply.github.com>
2024-11-22 17:48:11 +0700

    Update BoardConfig.h

--- configs/Sz's Corner V2/BoardConfig.h
+++ configs/Sz's Corner V2/BoardConfig.h
@@ -9,7 +9,7 @@
 #include "enums.pb.h"
 #include "class/hid/hid.h"
 
-#define BOARD_CONFIG_LABEL "Flatbox SZ v2"
+#define BOARD_CONFIG_LABEL "SZ Corner Flatbox v2"
 
 // Main pin mapping Configuration
 //                                                  // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
@@ -81,19 +81,20 @@
 #define LEDS_DPAD_LEFT   0
 #define LEDS_DPAD_DOWN   1
 #define LEDS_DPAD_RIGHT  2
-#define LEDS_DPAD_UP     3
-#define LEDS_BUTTON_B3   4
-#define LEDS_BUTTON_B4   5
-#define LEDS_BUTTON_R1   6
-#define LEDS_BUTTON_L1   7
-#define LEDS_BUTTON_B1   8
+#define LEDS_BUTTON_B3   3
+#define LEDS_BUTTON_B4   4
+#define LEDS_BUTTON_R1   5
+#define LEDS_BUTTON_L1   6
+#define LEDS_BUTTON_L2   7
+#define LEDS_BUTTON_R2   8
 #define LEDS_BUTTON_B2   9
-#define LEDS_BUTTON_R2   10
-#define LEDS_BUTTON_L2   11
-#define LEDS_BUTTON_A1   12
+#define LEDS_BUTTON_B1   10
+#define LEDS_DPAD_UP     11
+#define LEDS_BUTTON_R3   12
 #define LEDS_BUTTON_L3   13
-#define LEDS_BUTTON_R3   14
-#define LEDS_BUTTON_A2   15
+#define LEDS_BUTTON_A2   14
+#define LEDS_BUTTON_A1   15
+
 
 #define HAS_I2C_DISPLAY 1
 #define I2C0_ENABLED 1
