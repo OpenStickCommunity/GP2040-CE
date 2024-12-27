73a4694ab268e74c2f398c703d133e1391375439
ian <ian@vidales.se>
2024-12-28 00:17:09 +0100

    Prepare a system stats global state for future "new version banner"

---
+++ www/src/Store/useSystemStats.ts
@@ -0,0 +1,107 @@
+import { create } from 'zustand';
+import { baseUrl } from '../Services/WebApi';
+
+const percentage = (x, y) => parseFloat(((x / y) * 100).toFixed(2));
+const toKB = (x) => parseFloat((x / 1024).toFixed(2));
+
+type State = {
+	latestVersion: string;
+	latestDownloadUrl: string;
+	currentVersion: string;
+	boardConfigProperties: {
+		label: string;
+		fileName: string;
+	};
+	memoryReport: {
+		totalFlash: number;
+		usedFlash: number;
+		staticAllocs: number;
+		totalHeap: number;
+		usedHeap: number;
+		percentageFlash: number;
+		percentageHeap: number;
+	};
+	loading: boolean;
+	error: boolean;
+};
+
+type Actions = {
+	getSystemStats: () => void;
+};
+
+const INITIAL_STATE: State = {
+	latestVersion: '',
+	latestDownloadUrl: '',
+	currentVersion: '',
+	boardConfigProperties: {
+		label: '',
+		fileName: '',
+	},
+	memoryReport: {
+		totalFlash: 0,
+		usedFlash: 0,
+		staticAllocs: 0,
+		totalHeap: 0,
+		usedHeap: 0,
+		percentageFlash: 0,
+		percentageHeap: 0,
+	},
+	loading: false,
+	error: false,
+};
+
+const useSystemStats = create<State & Actions>()((set) => ({
+	...INITIAL_STATE,
+	getSystemStats: async () => {
+		set({ loading: true });
+
+		try {
+			const [firmwareVersion, memoryReport, latestRelease] = await Promise.all([
+				fetch(`${baseUrl}/api/getFirmwareVersion`).then((res) => res.json()),
+				fetch(`${baseUrl}/api/getMemoryReport`).then((res) => res.json()),
+				fetch(
+					'https://api.github.com/repos/OpenStickCommunity/GP2040-CE/releases/latest',
+				).then((res) => res.json()),
+			]);
+			const latestDownloadUrl =
+				latestRelease.assets?.find(
+					({ name }) =>
+						name
+							?.substring(name.lastIndexOf('_') + 1)
+							?.replace('.uf2', '')
+							?.toLowerCase() === firmwareVersion.boardConfig.toLowerCase(),
+				)?.browser_download_url ||
+				`https://github.com/OpenStickCommunity/GP2040-CE/releases/tag/${latestRelease.data.tag_name}`;
+
+			set({
+				currentVersion: firmwareVersion.version,
+				latestVersion: latestRelease.tag_name,
+				latestDownloadUrl,
+				boardConfigProperties: {
+					label: firmwareVersion.boardConfigLabel,
+					fileName: firmwareVersion.boardConfigFileName,
+				},
+				memoryReport: {
+					totalFlash: toKB(memoryReport.totalFlash),
+					usedFlash: toKB(memoryReport.usedFlash),
+					staticAllocs: toKB(memoryReport.staticAllocs),
+					totalHeap: toKB(memoryReport.totalHeap),
+					usedHeap: toKB(memoryReport.usedHeap),
+					percentageFlash: percentage(
+						memoryReport.usedFlash,
+						memoryReport.totalFlash,
+					),
+					percentageHeap: percentage(
+						memoryReport.usedHeap,
+						memoryReport.totalHeap,
+					),
+				},
+				loading: false,
+			});
+		} catch (error) {
+			set({ error: true, loading: false });
+		}
+	},
+}));
+
+export default useSystemStats;
