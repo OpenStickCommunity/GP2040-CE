0ff2274589f26b67b5cd6e030a9eba4a09c0e4be
SavageCore <talk@savagecore.uk>
2025-03-03 17:59:08 +0000

    Initial

---
+++ configs/VenomEZ/BoardConfig.h
@@ -0,0 +1,123 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2024 OpenStickCommunity (gp2040-ce.info)
+ */
+
+#ifndef VENOMEZ_CONFIG_H_
+#define VENOMEZ_CONFIG_H_
+
+#include "enums.pb.h"
+
+#define BOARD_CONFIG_LABEL "VenomEZ"
+
+// Main pin mapping Configuration
+//                          // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
+#define GPIO_PIN_17 GpioAction::BUTTON_PRESS_UP		 // UP     | UP     | UP      | UP       | UP     | UP     |
+#define GPIO_PIN_16 GpioAction::BUTTON_PRESS_DOWN	 // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
+#define GPIO_PIN_15 GpioAction::BUTTON_PRESS_RIGHT // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
+#define GPIO_PIN_14 GpioAction::BUTTON_PRESS_LEFT	 // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
+#define GPIO_PIN_05 GpioAction::BUTTON_PRESS_B1		 // B1     | A      | B       | Cross    | 2      | K1     |
+#define GPIO_PIN_06 GpioAction::BUTTON_PRESS_B2		 // B2     | B      | A       | Circle   | 3      | K2     |
+#define GPIO_PIN_13 GpioAction::BUTTON_PRESS_R2		 // R2     | RT     | ZR      | R2       | 8      | K3     |
+#define GPIO_PIN_11 GpioAction::BUTTON_PRESS_L2		 // L2     | LT     | ZL      | L2       | 7      | K4     |
+#define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B3		 // B3     | X      | Y       | Square   | 1      | P1     |
+#define GPIO_PIN_08 GpioAction::BUTTON_PRESS_B4		 // B4     | Y      | X       | Triangle | 4      | P2     |
+#define GPIO_PIN_12 GpioAction::BUTTON_PRESS_R1		 // R1     | RB     | R       | R1       | 6      | P3     |
+#define GPIO_PIN_10 GpioAction::BUTTON_PRESS_L1		 // L1     | LB     | L       | L1       | 5      | P4     |
+#define GPIO_PIN_19 GpioAction::BUTTON_PRESS_S1		 // S1     | Back   | Minus   | Select   | 9      | Coin   |
+#define GPIO_PIN_18 GpioAction::BUTTON_PRESS_S2		 // S2     | Start  | Plus    | Start    | 10     | Start  |
+// #define GPIO_PIN_XX GpioAction::BUTTON_PRESS_L3     // L3     | LS     | LS      | L3       | 11     | LS     |
+// #define GPIO_PIN_XX GpioAction::BUTTON_PRESS_R3     // R3     | RS     | RS      | R3       | 12     | RS     |
+#define GPIO_PIN_09 GpioAction::BUTTON_PRESS_A1 // A1     | Guide  | Home    | PS       | 13     | ~      |
+#define GPIO_PIN_20 GpioAction::BUTTON_PRESS_A2 // A2     | ~      | Capture | ~        | 14     | ~      |
+
+// Keyboard Mapping Configuration
+//                                            // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
+#define KEY_DPAD_UP HID_KEY_ARROW_UP			 // UP     | UP     | UP      | UP       | UP     | UP     |
+#define KEY_DPAD_DOWN HID_KEY_ARROW_DOWN	 // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
+#define KEY_DPAD_RIGHT HID_KEY_ARROW_RIGHT // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
+#define KEY_DPAD_LEFT HID_KEY_ARROW_LEFT	 // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
+#define KEY_BUTTON_B1 HID_KEY_SHIFT_LEFT	 // B1     | A      | B       | Cross    | 2      | K1     |
+#define KEY_BUTTON_B2 HID_KEY_Z						 // B2     | B      | A       | Circle   | 3      | K2     |
+#define KEY_BUTTON_R2 HID_KEY_X						 // R2     | RT     | ZR      | R2       | 8      | K3     |
+#define KEY_BUTTON_L2 HID_KEY_V						 // L2     | LT     | ZL      | L2       | 7      | K4     |
+#define KEY_BUTTON_B3 HID_KEY_CONTROL_LEFT // B3     | X      | Y       | Square   | 1      | P1     |
+#define KEY_BUTTON_B4 HID_KEY_ALT_LEFT		 // B4     | Y      | X       | Triangle | 4      | P2     |
+#define KEY_BUTTON_R1 HID_KEY_SPACE				 // R1     | RB     | R       | R1       | 6      | P3     |
+#define KEY_BUTTON_L1 HID_KEY_C						 // L1     | LB     | L       | L1       | 5      | P4     |
+#define KEY_BUTTON_S1 HID_KEY_5						 // S1     | Back   | Minus   | Select   | 9      | Coin   |
+#define KEY_BUTTON_S2 HID_KEY_1						 // S2     | Start  | Plus    | Start    | 10     | Start  |
+#define KEY_BUTTON_L3 HID_KEY_EQUAL				 // L3     | LS     | LS      | L3       | 11     | LS     |
+#define KEY_BUTTON_R3 HID_KEY_MINUS				 // R3     | RS     | RS      | R3       | 12     | RS     |
+#define KEY_BUTTON_A1 HID_KEY_9						 // A1     | Guide  | Home    | PS       | 13     | ~      |
+#define KEY_BUTTON_A2 HID_KEY_F2					 // A2     | ~      | Capture | ~        | 14     | ~      |
+#define KEY_BUTTON_FN -1									 // Hotkey Function                                        |
+
+#define TURBO_ENABLED 1
+#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_TURBO
+
+#define JSLIDER_ENABLED 1
+#define GPIO_PIN_22 GpioAction::SUSTAIN_DP_MODE_DP
+// #define GPIO_PIN_22 GpioAction::SUSTAIN_DP_MODE_RS
+#define SLIDER_MODE_ZERO DPAD_MODE_LEFT_ANALOG
+
+#define SLIDER_SOCD_ENABLED 0
+// #define GPIO_PIN_XX GpioAction::SUSTAIN_SOCD_MODE_UP_PRIO
+// #define GPIO_PIN_XX GpioAction::SUSTAIN_SOCD_MODE_SECOND_WIN
+// #define SLIDER_SOCD_SLOT_DEFAULT SOCD_MODE_NEUTRAL
+
+#define DEFAULT_PS4CONTROLLER_TYPE PS4_ARCADESTICK
+
+#define DEFAULT_INPUT_MODE_R1 INPUT_MODE_XINPUT
+
+#define BOARD_LEDS_PIN -1
+#define TURBO_LED_PIN 29
+
+#define LED_BRIGHTNESS_MAXIMUM 50
+#define LEDS_PER_PIXEL 2
+
+#define LEDS_DPAD_LEFT 0
+#define LEDS_DPAD_DOWN 1
+#define LEDS_DPAD_RIGHT 2
+#define LEDS_BUTTON_B3 3
+#define LEDS_BUTTON_B4 4
+#define LEDS_BUTTON_R1 5
+#define LEDS_BUTTON_L1 6
+#define LEDS_BUTTON_L2 7
+#define LEDS_BUTTON_R2 8
+#define LEDS_BUTTON_B2 9
+#define LEDS_BUTTON_B1 10
+#define LEDS_DPAD_UP 11
+
+#define PLED_TYPE PLED_TYPE_PWM
+#define PLED1_PIN 25
+#define PLED2_PIN 26
+#define PLED3_PIN 27
+#define PLED4_PIN 28
+// #define PLED_COLOR ColorGreen
+
+#define HAS_I2C_DISPLAY 0
+// #define I2C_SDA_PIN 0
+// #define I2C_SCL_PIN 1
+// #define DISPLAY_FLIP 3
+
+#define REVERSE_UP_DEFAULT 1
+#define REVERSE_DOWN_DEFAULT 1
+#define REVERSE_LEFT_DEFAULT 1
+#define REVERSE_RIGHT_DEFAULT 1
+
+#define BUTTON_LAYOUT BUTTON_LAYOUT_STICK
+#define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_SEGA2P
+#define SPLASH_MODE SPLASH_MODE_NONE
+
+#define FOCUS_MODE_ENABLED 1
+#define FOCUS_MODE_BUTTON_MASK GAMEPAD_MASK_S1 | GAMEPAD_MASK_S2 | GAMEPAD_MASK_A1 | GAMEPAD_MASK_A2 | GAMEPAD_MASK_L3 | GAMEPAD_MASK_R3
+#define FOCUS_MODE_PIN 24
+#define FOCUS_MODE_BUTTON_LOCK_ENABLED 1
+
+#define PSPASSTHROUGH_ENABLED 0
+// #define PSPASSTHROUGH_PIN_DPLUS 28
+
+#define XBONEPASSTHROUGH_ENABLED 0
+
+#endif
---
+++ configs/VenomEZ/Readme.md
@@ -0,0 +1,5 @@
+# GP2040 Configuration for VenomEZ
+
+![VenomEZ](assets/VenomEZ.jpg)
+
+Configuration for the [VenomEZ](https://github.com/SavageCore/VenomEZ).
