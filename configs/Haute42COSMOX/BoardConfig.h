07e437c5a69aeb2b1919e848d85ecb0514981348
Jesse Zhao <jesse.zhao@epicgames.com>
2025-03-25 09:48:25 -0700

    Build on j-zhao branch, mustfun board

--- configs/Haute42COSMOX/BoardConfig.h
+++ configs/Haute42COSMOX/BoardConfig.h
@@ -5,43 +5,42 @@
 
  #ifndef PICO_BOARD_CONFIG_H_
  #define PICO_BOARD_CONFIG_H_
- 
+
  #include "enums.pb.h"
  #include "class/hid/hid.h"
- 
+
  #define BOARD_CONFIG_LABEL "Haute42 COSMOX"
- 
+
  // Main pin mapping Configuration
- //                          // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
- #define GPIO_PIN_02 GpioAction::BUTTON_PRESS_UP     // UP     | UP     | UP      | UP       | UP     | UP     |
- #define GPIO_PIN_03 GpioAction::BUTTON_PRESS_DOWN   // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
- #define GPIO_PIN_04 GpioAction::BUTTON_PRESS_RIGHT  // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
- #define GPIO_PIN_05 GpioAction::BUTTON_PRESS_LEFT   // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
- #define GPIO_PIN_06 GpioAction::BUTTON_PRESS_B1     // B1     | A      | B       | Cross    | 2      | K1     |
- #define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B2     // B2     | B      | A       | Circle   | 3      | K2     |
- #define GPIO_PIN_08 GpioAction::BUTTON_PRESS_R2     // R2     | RT     | ZR      | R2       | 8      | K3     |
- #define GPIO_PIN_09 GpioAction::BUTTON_PRESS_L2     // L2     | LT     | ZL      | L2       | 7      | K4     |
- #define GPIO_PIN_10 GpioAction::BUTTON_PRESS_B3     // B3     | X      | Y       | Square   | 1      | P1     |
- #define GPIO_PIN_11 GpioAction::BUTTON_PRESS_B4     // B4     | Y      | X       | Triangle | 4      | P2     |
- #define GPIO_PIN_12 GpioAction::BUTTON_PRESS_R1     // R1     | RB     | R       | R1       | 6      | P3     |
- #define GPIO_PIN_13 GpioAction::BUTTON_PRESS_L1     // L1     | LB     | L       | L1       | 5      | P4     |
- #define GPIO_PIN_16 GpioAction::BUTTON_PRESS_S1     // S1     | Back   | Minus   | Select   | 9      | Coin   |
- #define GPIO_PIN_17 GpioAction::BUTTON_PRESS_S2     // S2     | Start  | Plus    | Start    | 10     | Start  |
- #define GPIO_PIN_18 GpioAction::BUTTON_PRESS_L3     // L3     | LS     | LS      | L3       | 11     | LS     |
- #define GPIO_PIN_19 GpioAction::BUTTON_PRESS_R3     // R3     | RS     | RS      | R3       | 12     | RS     |
- #define GPIO_PIN_20 GpioAction::BUTTON_PRESS_A1     // A1     | Guide  | Home    | PS       | 13     | ~      |
- #define GPIO_PIN_21 GpioAction::BUTTON_PRESS_A2     // A2     | ~      | Capture | ~        | 14     | ~      |
- #define GPIO_PIN_27 GpioAction::BUTTON_PRESS_UP     // UP     | UP     | UP      | UP       | UP     | UP     |
- #define GPIO_PIN_26 GpioAction::BUTTON_PRESS_L3     // L3     | LS     | LS      | L3       | 11     | LS     |
- 
+ //                                                  // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
+ #define GPIO_PIN_17 GpioAction::BUTTON_PRESS_UP     // UP     | UP     | UP      | UP       | UP     | UP     |
+ #define GPIO_PIN_15 GpioAction::BUTTON_PRESS_DOWN   // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
+ #define GPIO_PIN_14 GpioAction::BUTTON_PRESS_RIGHT  // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
+ #define GPIO_PIN_16 GpioAction::BUTTON_PRESS_LEFT   // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
+ #define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B1     // B1     | A      | B       | Cross    | 2      | K1     |
+ #define GPIO_PIN_08 GpioAction::BUTTON_PRESS_B2     // B2     | B      | A       | Circle   | 3      | K2     |
+ #define GPIO_PIN_09 GpioAction::BUTTON_PRESS_R2     // R2     | RT     | ZR      | R2       | 8      | K3     |
+ #define GPIO_PIN_10 GpioAction::BUTTON_PRESS_L2     // L2     | LT     | ZL      | L2       | 7      | K4     |
+ #define GPIO_PIN_03 GpioAction::BUTTON_PRESS_B3     // B3     | X      | Y       | Square   | 1      | P1     |
+ #define GPIO_PIN_04 GpioAction::BUTTON_PRESS_B4     // B4     | Y      | X       | Triangle | 4      | P2     |
+ #define GPIO_PIN_05 GpioAction::BUTTON_PRESS_R1     // R1     | RB     | R       | R1       | 6      | P3     |
+ #define GPIO_PIN_06 GpioAction::BUTTON_PRESS_L1     // L1     | LB     | L       | L1       | 5      | P4     |
+ #define GPIO_PIN_21 GpioAction::BUTTON_PRESS_S1     // S1     | Back   | Minus   | Select   | 9      | Coin   |
+ #define GPIO_PIN_23 GpioAction::BUTTON_PRESS_S2     // S2     | Start  | Plus    | Start    | 10     | Start  |
+ #define GPIO_PIN_02 GpioAction::BUTTON_PRESS_L3     // L3     | LS     | LS      | L3       | 11     | LS     |
+ #define GPIO_PIN_11 GpioAction::BUTTON_PRESS_R3     // R3     | RS     | RS      | R3       | 12     | RS     |
+ #define GPIO_PIN_24 GpioAction::BUTTON_PRESS_A1     // A1     | Guide  | Home    | PS       | 13     | ~      |
+ #define GPIO_PIN_20 GpioAction::BUTTON_PRESS_A2     // A2     | ~      | Capture | ~        | 14     | ~      |
+ #define GPIO_PIN_12 GpioAction::BUTTON_PRESS_UP     // UP     | UP     | UP      | UP       | UP     | UP     |
+
  // Setting GPIO pins to assigned by add-on
  //
- #define GPIO_PIN_00 GpioAction::ASSIGNED_TO_ADDON
- #define GPIO_PIN_01 GpioAction::ASSIGNED_TO_ADDON
- #define GPIO_PIN_23 GpioAction::ASSIGNED_TO_ADDON
- #define GPIO_PIN_24 GpioAction::ASSIGNED_TO_ADDON
- #define GPIO_PIN_28 GpioAction::ASSIGNED_TO_ADDON
- 
+ // #define GPIO_PIN_00 GpioAction::ASSIGNED_TO_ADDON
+ // #define GPIO_PIN_01 GpioAction::ASSIGNED_TO_ADDON
+ // #define GPIO_PIN_23 GpioAction::ASSIGNED_TO_ADDON
+ // #define GPIO_PIN_24 GpioAction::ASSIGNED_TO_ADDON
+ // #define GPIO_PIN_28 GpioAction::ASSIGNED_TO_ADDON
+
  // Keyboard Mapping Configuration
  //                                            // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
  #define KEY_DPAD_UP     HID_KEY_ARROW_UP      // UP     | UP     | UP      | UP       | UP     | UP     |
@@ -63,19 +62,19 @@
  #define KEY_BUTTON_A1   HID_KEY_9             // A1     | Guide  | Home    | PS       | 13     | ~      |
  #define KEY_BUTTON_A2   HID_KEY_F2            // A2     | ~      | Capture | ~        | 14     | ~      |
  #define KEY_BUTTON_FN   -1                    // Hotkey Function                                        |
- 
+
  #define USB_PERIPHERAL_ENABLED 1
- 
+
  #define USB_PERIPHERAL_PIN_DPLUS 23
  #define USB_PERIPHERAL_PIN_ORDER 0
- 
+
  #define DEFAULT_INPUT_MODE_R1 INPUT_MODE_XBONE
  #define DEFAULT_INPUT_MODE_B4 INPUT_MODE_PS5
  #define DEFAULT_PS5AUTHENTICATION_TYPE INPUT_MODE_AUTH_TYPE_USB
- 
+
  #define TURBO_ENABLED 1
  #define GPIO_PIN_14 GpioAction::BUTTON_PRESS_TURBO
- 
+
  #define BOARD_LEDS_PIN 28
  #define LED_BRIGHTNESS_MAXIMUM 100
  #define LED_BRIGHTNESS_STEPS 5
@@ -98,17 +97,17 @@
  #define LEDS_BUTTON_L3   13
  #define LEDS_BUTTON_R3   14
  #define LEDS_BUTTON_A2   15
- 
+
  #define HAS_I2C_DISPLAY 1
  #define I2C0_ENABLED 1
  #define I2C0_PIN_SDA 0
  #define I2C0_PIN_SCL 1
  #define SPLASH_MODE SPLASH_MODE_STATIC
  #define SPLASH_DURATION 3000
- 
+
  #define BUTTON_LAYOUT BUTTON_LAYOUT_BOARD_DEFINED_A
  #define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_BOARD_DEFINED_B
- 
+
  // Stickless
  #define DEFAULT_BOARD_LAYOUT_A {\
      {GP_ELEMENT_PIN_BUTTON, {8,  20, 8, 8, 1, 1, 5,     GP_SHAPE_ELLIPSE}},\
@@ -116,7 +115,7 @@
      {GP_ELEMENT_PIN_BUTTON, {41, 29, 8, 8, 1, 1, 4,     GP_SHAPE_ELLIPSE}},\
      {GP_ELEMENT_PIN_BUTTON, {48, 53, 8, 8, 1, 1, 2,     GP_SHAPE_ELLIPSE}}\
  }
- 
+
  #define DEFAULT_BOARD_LAYOUT_B {\
      {GP_ELEMENT_PIN_BUTTON, {57,  20, 8, 8, 1, 1, 10,   GP_SHAPE_ELLIPSE}},\
      {GP_ELEMENT_PIN_BUTTON, {75,  16, 8, 8, 1, 1, 11,   GP_SHAPE_ELLIPSE}},\
@@ -262,13 +261,13 @@
      {GP_ELEMENT_PIN_BUTTON, {79, 35, 4, 4, 1, 1, 8,     GP_SHAPE_ELLIPSE}},\
      {GP_ELEMENT_PIN_BUTTON, {89, 37, 4, 4, 1, 1, 9,     GP_SHAPE_ELLIPSE}}\
 }
- 
+
  // Keyboard Host enabled by default
  #define KEYBOARD_HOST_ENABLED 1
- 
+
  #define BOARD_LED_ENABLED 1
  #define BOARD_LED_TYPE ON_BOARD_LED_MODE_MODE_INDICATOR
- 
+
  #define DEFAULT_SPLASH \
  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
@@ -334,5 +333,5 @@
  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
- 
+
  #endif
