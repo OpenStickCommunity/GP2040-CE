5ebc8d505164ff8f52ceae00c7a2c198c141f750
mizma <omoikane@path-works.net>
2025-04-08 21:57:56 +0900

    Implement all options and refactor
    - Implement the --to and --from option. can now specify how many commit
    	history against the target locale directory to go back to.
    - Refactored code.
    - Clean up some unnecessary/unused variables.

--- www/scripts/checklocale.js
+++ www/scripts/checklocale.js
@@ -1,6 +1,5 @@
 import { execSync } from "child_process";
 import { spawn } from "child_process";
-import { Hash } from "crypto";
 import { parseArgs } from "node:util";
 
 function printUsage() {
@@ -18,105 +17,123 @@ function printUsage() {
 	console.log("  -t|--to <history> which commit to diff against (0 = HEAD)");
 }
 
-const options = {
-	"locale": {
-		type: "string",
-		short: "l",
-	},
-	"from": {
-		type: "string",
-		short: "f",
-		default: "1",
-	},
-	"to": {
-		type: "string",
-		short: "t",
-		default: "0",
-	},
-};
-
-const args = parseArgs({ options });
-
-if (!args.values.locale) {
-	console.log("Error: The --locale option is required");
-	printUsage();
-	process.exit(1);
+function getTargetDir(locale) {
+	switch (locale) {
+		case "de":
+		case "de-DE":
+			return "./src/Locales/de-DE/";
+		case "ja":
+		case "ja-JP":
+			return "./src/Locales/ja-JP/";
+		case "ko":
+		case "ko-KR":
+			return "./src/Locales/ko-KR/";
+		case "pt":
+		case "pt-BR":
+			return "./src/Locales/pt-BR/";
+		case "zh":
+		case "zh-CN":
+			return "./src/Locales/zh-CN/";
+		default:
+			console.log(`${locale} is not a valid locale`);
+			printUsage();
+			process.exit(1);
+	}
 }
 
-//TODO: parse --from annd --to
-console.log(`from : ${args.values.from}`);
-console.log(`to   : ${args.values.to}`);
+function getHashArry(targetDir) {
+	let hashArry;
+	try {
+		// get diff hashes (max 100 to avoid tracking unnecessarily long history)
+		let CommitHashes = execSync(`git log -n 100 --pretty=format:%H -- ${targetDir}`)
+			.toString().trim();
+		hashArry = CommitHashes.split("\n").map(line => line.trim()).filter(line => line !== "");
+		hashArry.unshift("HEAD");
 
-let targetDir;
+		if (!hashArry) {
+			console.error(`No commits found for the directory: ${targetDir}`);
+			process.exit(1);
+		}
+	} catch (error) {
+		console.error(`Error executing git commands: ${error.message}`);
+		process.exit(1);
+	}
+	return hashArry;
+}
 
-switch (args.values.locale) {
-	case "de":
-	case "de-DE":
-		targetDir = "./src/Locales/de-DE/";
-		break;
-	case "ja":
-	case "ja-JP":
-		targetDir = "./src/Locales/ja-JP/";
-		break;
-	case "ko":
-	case "ko-KR":
-		targetDir = "./src/Locales/ko-KR/";
-		break;
-	case "pt":
-	case "pt-BR":
-		targetDir = "./src/Locales/pt-BR/";
-		break;
-	case "zh":
-	case "zh-CN":
-		targetDir = "./src/Locales/zh-CN/";
-		break;
-	default:
-		console.log(`${args.values.locale} is not a valid locale`);
+function getDiff(fromHash, toHash, diffPath) {
+	console.log(
+		`Running git diff from commit ${fromHash} to ${toHash} for ${diffPath}...`,
+	);
+	let fromLog = execSync(`git log -1 --format="%an <%ae> %ad %n%B" ${fromHash}`).toString().trim();
+	let toLog = execSync(`git log -1 --format="%an <%ae> %ad %n%B" ${toHash}`).toString().trim();
+
+	console.log(`commit log of ${fromHash}:`)
+	console.log(fromLog);
+	console.log(`commit log of ${toHash}:`)
+	console.log(toLog);
+
+	const gitDiff = spawn(
+		"git",
+		["diff", fromHash, toHash, "--", diffPath],
+		{ stdio: "inherit" },
+	);
+
+	gitDiff.on("error", (err) => {
+		console.error(`Error executing git diff: ${err.message}`);
+		process.exit(1);
+	});
+
+	gitDiff.on("close", (code) => {
+		if (code !== 0) {
+			console.error(`git diff process exited with code ${code}`);
+			process.exit(code);
+		}
+	});
+}
+
+function checkLocale() {
+	const options = {
+		"locale": {
+			type: "string",
+			short: "l",
+		},
+		"from": {
+			type: "string",
+			short: "f",
+			default: "1",
+		},
+		"to": {
+			type: "string",
+			short: "t",
+			default: "0",
+		},
+	};
+	const args = parseArgs({ options });
+	const diffPath = "./src/Locales/en/";
+
+	let targetDir;
+	let hashArry;
+	let diffFrom;
+	let diffTo;
+
+	if (!args.values.locale) {
+		console.log("Error: The --locale option is required");
 		printUsage();
 		process.exit(1);
+	}
+
+	// set the target diff history id
+	diffFrom = parseInt(args.values.from, 10);
+	diffTo = parseInt(args.values.to, 10);
+
+	// get target directory for the locale
+	targetDir = getTargetDir(args.values.locale)
+
+	// get list of commit hashes for targetDir
+	hashArry = getHashArry(targetDir);
+
+	getDiff(hashArry[diffFrom], hashArry[diffTo], diffPath);
 }
 
-const diffPath = "./src/Locales/en/";
-
-let latestCommitHash;
-let CommitHashes; // for development
-
-try {
-	// Find the latest commit hash that modified the target locale directory
-	latestCommitHash = execSync(`git log -n 1 --pretty=format:%H -- ${targetDir}`)
-		.toString().trim();
-
-	CommitHashes = execSync(`git log -n 100 --pretty=format:%H -- ${targetDir}`)
-		.toString().trim();
-	let HashArry = CommitHashes.split("\n").map(line => line.trim()).filter(line => line !== "");
-	console.log(HashArry);
-
-	if (!latestCommitHash) {
-		console.error(`No commits found for the directory: ${targetDir}`);
-		process.exit(1);
-	}
-} catch (error) {
-	console.error(`Error executing git commands: ${error.message}`);
-	process.exit(1);
-}
-
-console.log(
-	`Running git diff from commit ${latestCommitHash} to HEAD for ${diffPath}...`,
-);
-const gitDiff = spawn(
-	"git",
-	["diff", latestCommitHash, "HEAD", "--", diffPath],
-	{ stdio: "inherit" },
-);
-
-gitDiff.on("error", (err) => {
-	console.error(`Error executing git diff: ${err.message}`);
-	process.exit(1);
-});
-
-gitDiff.on("close", (code) => {
-	if (code !== 0) {
-		console.error(`git diff process exited with code ${code}`);
-		process.exit(code);
-	}
-});
+checkLocale();
