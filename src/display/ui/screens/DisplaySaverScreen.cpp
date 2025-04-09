4974a48fad6b305a37f6b2239ee017d00ef024bf
Luke A <arntsonl@gmail.com>
2025-04-09 11:07:03 -0400

    Fix for toast and bounce screen saver logos

--- src/display/ui/screens/DisplaySaverScreen.cpp
+++ src/display/ui/screens/DisplaySaverScreen.cpp
@@ -1,4 +1,5 @@
 #include "DisplaySaverScreen.h"
+#include "BitmapScreens.h"
 
 #include "drivermanager.h"
 #include "pico/stdlib.h"
@@ -21,6 +22,8 @@ void DisplaySaverScreen::init() {
         case DisplaySaverMode::DISPLAY_SAVER_TOAST:
             initToasters();
             break;
+        default:
+            break;
     }
 }
 
@@ -41,6 +44,8 @@ void DisplaySaverScreen::drawScreen() {
             break;
         case DisplaySaverMode::DISPLAY_SAVER_TOAST:
             drawToasterScene();
+            break;
+        default:
             break;
     }
 }
@@ -118,7 +123,7 @@ void DisplaySaverScreen::drawBounceScene() {
 
     if (bounceSpriteY <= 0 || bounceSpriteY + scaledHeight >= SCREEN_HEIGHT) bounceSpriteVelocityY = -bounceSpriteVelocityY;
 
-    getRenderer()->drawSprite((uint8_t *)getDisplayOptions().splashImage.bytes, bounceSpriteWidth, bounceSpriteHeight, 0, bounceSpriteX, bounceSpriteY, 0, bounceScale);
+    getRenderer()->drawSprite((uint8_t *)bitmapGP2040Logo, bounceSpriteWidth, bounceSpriteHeight, 0, bounceSpriteX, bounceSpriteY, 0, bounceScale);
 }
 
 void DisplaySaverScreen::drawPipeScene() {
@@ -161,8 +166,8 @@ void DisplaySaverScreen::drawPipeScene() {
 void DisplaySaverScreen::initToasters() {
     for (uint16_t i = 0; i < numberOfToasters; ++i) {
         double scale = 1.0 / ((i/2)+2);
-        int16_t dx = (-1 - rand() % 3);
-        int16_t dy = (1 + rand() % 3);
+        int16_t dx = (-1 - rand() % 2);
+        int16_t dy = (1 + rand() % 2);
 
         toasters.push_back({
             scale,
@@ -178,7 +183,7 @@ void DisplaySaverScreen::drawToasterScene() {
     for (uint16_t i = 0; i < toasters.size(); ++i) {
         ToastParams& sprite = toasters[i];
 
-        getRenderer()->drawSprite((uint8_t *) getDisplayOptions().splashImage.bytes, toasterSpriteWidth, toasterSpriteHeight, 0, sprite.x, sprite.y, 0, sprite.scale);
+        getRenderer()->drawSprite((uint8_t *)bitmapGP2040Logo, toasterSpriteWidth, toasterSpriteHeight, 0, sprite.x, sprite.y, 0, sprite.scale);
 
         sprite.x += sprite.dx;
         sprite.y += sprite.dy;
@@ -186,8 +191,8 @@ void DisplaySaverScreen::drawToasterScene() {
         if (sprite.x + toasterSpriteWidth * sprite.scale < 0) {
             sprite.x = SCREEN_WIDTH;
             sprite.y = rand() % (SCREEN_HEIGHT - static_cast<int16_t>(toasterSpriteHeight * sprite.scale));
-            sprite.dx = (-1 - rand() % 3);
-            sprite.dy = (1 + rand() % 3);
+            sprite.dx = (-1 - rand() % 2);
+            sprite.dy = (1 + rand() % 2);
         }
 
         if (sprite.y > SCREEN_HEIGHT) {
