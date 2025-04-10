d4acd760a7dd208529153980de66d5f19158cef3
Luke A <arntsonl@gmail.com>
2025-04-09 22:24:00 -0400

    Fixed missing custom theme pressed lights

--- src/animationstation/effects/customthemepressed.cpp
+++ src/animationstation/effects/customthemepressed.cpp
@@ -31,6 +31,29 @@ CustomThemePressed::CustomThemePressed(PixelMatrix &matrix) : Animation(matrix) 
 
 CustomThemePressed::CustomThemePressed(PixelMatrix &matrix, std::vector<Pixel> &pixels) : Animation(matrix), pixels(&pixels) {
   this->filtered = true;
+
+  AnimationOptions & animationOptions = Storage::getInstance().getAnimationOptions();
+  if (animationOptions.hasCustomTheme)
+	{
+      theme[GAMEPAD_MASK_DU] = RGB(animationOptions.customThemeUpPressed);
+      theme[GAMEPAD_MASK_DD] = RGB(animationOptions.customThemeDownPressed);
+      theme[GAMEPAD_MASK_DL] = RGB(animationOptions.customThemeLeftPressed);
+      theme[GAMEPAD_MASK_DR] = RGB(animationOptions.customThemeRightPressed);
+      theme[GAMEPAD_MASK_B1] = RGB(animationOptions.customThemeB1Pressed);
+      theme[GAMEPAD_MASK_B2] = RGB(animationOptions.customThemeB2Pressed);
+      theme[GAMEPAD_MASK_B3] = RGB(animationOptions.customThemeB3Pressed);
+      theme[GAMEPAD_MASK_B4] = RGB(animationOptions.customThemeB4Pressed);
+      theme[GAMEPAD_MASK_L1] = RGB(animationOptions.customThemeL1Pressed);
+      theme[GAMEPAD_MASK_R1] = RGB(animationOptions.customThemeR1Pressed);
+      theme[GAMEPAD_MASK_L2] = RGB(animationOptions.customThemeL2Pressed);
+      theme[GAMEPAD_MASK_R2] = RGB(animationOptions.customThemeR2Pressed);
+      theme[GAMEPAD_MASK_S1] = RGB(animationOptions.customThemeS1Pressed);
+      theme[GAMEPAD_MASK_S2] = RGB(animationOptions.customThemeS2Pressed);
+      theme[GAMEPAD_MASK_A1] = RGB(animationOptions.customThemeA1Pressed);
+      theme[GAMEPAD_MASK_A2] = RGB(animationOptions.customThemeA2Pressed);
+      theme[GAMEPAD_MASK_L3] = RGB(animationOptions.customThemeL3Pressed);
+      theme[GAMEPAD_MASK_R3] = RGB(animationOptions.customThemeR3Pressed);
+	}
 }
 
 bool CustomThemePressed::Animate(RGB (&frame)[100]) {
