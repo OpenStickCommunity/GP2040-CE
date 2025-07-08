766f159cb2f327af415a61412ee4391e946ce1cf
Ian <ian@vidales.se>
2025-07-08 20:36:23 +0200

    Remove unused env

--- www/env.d.ts
+++ www/env.d.ts
@@ -2,7 +2,6 @@
 
 interface ImportMetaEnv {
 	readonly VITE_GP2040_BOARD: string;
-	readonly VITE_GP2040_CONTROLLER: string;
 	readonly VITE_DEV_BASE_URL: string;
 	readonly VITE_DEV_HOST: string;
 }
