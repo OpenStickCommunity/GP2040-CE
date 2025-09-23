8d89fcbc97616a7a67352a1086b6a58f122bceb7
Joohan Lee <losernator@gmail.com>
2025-09-23 17:50:31 +0900

    Update ko-KR localization strings
    * Changed wording to match update

--- www/src/Locales/ko-KR/SettingsPage.jsx
+++ www/src/Locales/ko-KR/SettingsPage.jsx
@@ -81,7 +81,7 @@ export default {
 	'xbone-mode-text':
 		'<span>정보:</span> Xbox One 모드에서는 USB 장치에 인증을 위한 USB 동글 연결이 필요합니다.',
 	'xinput-mode-text':
-		'<span>정보:</span> Xinput은 별동의 인증이 필요없는 PC에서 사용가능합니다. Xbox360에서 사용하려면 USB인증을 선택하고 적절한 동글을 연결해주세요.',
+		'<span>정보:</span> Xinput은 동글 없이 Xbox360에서 사용가능합니다. 외부 동글을 이용해 인증하려는 경우에만 USB인증을 선택해주세요.',
 	'hotkey-settings-label': '핫키 설정',
 	'hotkey-settings-sub-header':
 		'<strong>Fn</strong> 슬라이더는 <link_pinmap>핀 매핑</link_pinmap> 페이지에서 설정 가능한 펑션 버튼(FN)을 활성화합니다. <strong>Fn</strong> 버튼을 활성화 하면 선택한 핫키 설정과 펑션 버튼을 함께 눌러야 합니다.<br />버튼 할당을 해제하려면 드롭다운 메뉴에서 <strong>없음</strong>을 선택하십시오.',
