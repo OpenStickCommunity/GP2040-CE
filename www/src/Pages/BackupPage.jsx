6e73809d20d9b6e360dceb962cb172a0e6662a47
Ian <ian@vidales.se>
2025-12-15 11:02:04 +0100

    Add keyboard mappings to export/import

--- www/src/Pages/BackupPage.jsx
+++ www/src/Pages/BackupPage.jsx
@@ -24,6 +24,10 @@ const API_BINDING = {
 		get: WebApi.getGamepadOptions,
 		set: WebApi.setGamepadOptions,
 	},
+	keyboard: {
+		get: WebApi.getKeyMappings,
+		set: WebApi.setKeyMappings,
+	},
 	led: { get: WebApi.getLedOptions, set: WebApi.setLedOptions },
 	ledTheme: {
 		get: WebApi.getCustomTheme,
