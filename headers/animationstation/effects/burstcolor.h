e7166ffb724f2ea985961a0afe692010c998baab
Podbod12 <pod@podrasoft.com>
2026-02-21 11:12:45 +0000

    Halved min time for bursts

--- headers/animationstation/effects/burstcolor.h
+++ headers/animationstation/effects/burstcolor.h
@@ -28,7 +28,7 @@ struct FGridEntry
 
 #define MAX_BURSTS 32
 #define BURST_DISTANCE 6
-#define MIN_TIME_BETWEEN_BURSTS_ON_BUTTON_IN_MS 100
+#define MIN_TIME_BETWEEN_BURSTS_ON_BUTTON_IN_MS 50
 #define BURST_DISTANCE_PER_SEC 10.0f
 
 class BurstColor : public Animation {
