66cbf01a4aaedc84bf41ad3251f84c72fb32ef64
Roderick MacCrimmon <r.maccrimm@gmail.com>
2026-03-05 16:27:12 -0700

    Refactoring components so there are fewer state subscriptions in the top-level BootModeMapping

--- www/src/Components/BootModeMapping.tsx
+++ www/src/Components/BootModeMapping.tsx
@@ -1,7 +1,7 @@
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { ReactNode, useContext, useEffect } from 'react';
 import Section from './Section';
-import { useBootModesStore, NUM_PINS } from '../Store/useBootModesStore';
+import { useBootModesStore, NUM_PINS, PinsKey } from '../Store/useBootModesStore';
 import { INPUT_MODE_OPTIONS, InputModeOptions } from '../Data/InputBootModes';
 import { AppContext } from '../Contexts/AppContext';
 import CustomSelect from './CustomSelect';
@@ -23,7 +23,10 @@ const groupedOptions = [
 
 function BootModeSelect({ mappingIndex }: { mappingIndex: number }) {
 	const setInputMode = useBootModesStore((state) => state.setInputMode);
+	const inputMode = useBootModesStore((state) => state.bootModes[mappingIndex].inputMode);
 	const { t } = useTranslation('');
+
+	const value = INPUT_MODE_OPTIONS.filter(({ value }) => value === inputMode);
 
 	const getOptionLabel = (option: InputModeOptions) =>
 		t(`SettingsPage:${option.labelKey}`);
@@ -40,6 +43,7 @@ function BootModeSelect({ mappingIndex }: { mappingIndex: number }) {
 			isDisabled={false}
 			getOptionLabel={getOptionLabel}
 			onChange={onChange}
+			value={value}
 		/>
 	);
 }
@@ -49,38 +53,49 @@ type PinOption = {
 	value: number;
 };
 
-function PinSelect({
-	onSelect,
-	onRemove,
-}: {
-	onSelect: (pin: number) => void;
-	onRemove: (pin: number) => void;
-}) {
+const pinOptions: PinOption[] = Array.from({ length: NUM_PINS }, (_, i) => ({
+	label: `GP${i}`,
+	value: i,
+}));
+
+function PinSelect({ pinsKey }: { pinsKey: PinsKey }) {
+	const addPin = useBootModesStore((state) => state.addPin);
+	const removePin = useBootModesStore((state) => state.removePin);
+	const pins = useBootModesStore((state) => {
+		if (pinsKey == 'usbModePins' || pinsKey == 'webConfigPins') {
+			return state[pinsKey];
+		}
+		return state.bootModes[pinsKey].pins;
+	});
+
+	const values = pinOptions.filter(({ value }) => pins.has(value));
+
 	const onChange = (_: MultiValue<PinOption>, action: ActionMeta<PinOption>) => {
 		if (!action.option?.value) {
 			return;
 		}
 		if (action.action === 'select-option') {
-			onSelect(action.option.value);
+			addPin(pinsKey, action.option.value);
 		} else if (action.action === 'remove-value') {
-			onRemove(action.option.value);
+			removePin(pinsKey, action.option.value);
 		}
 	};
-	const pins = Array.from({ length: NUM_PINS }, (_, i) => ({
-		label: `GP${i}`,
-		value: i,
-	}));
 
 	return (
 		<div className="d-flex gap-2">
 			<CustomSelect
 				isClearable={false}
 				isMulti={true}
-				options={pins}
+				options={pinOptions}
 				isDisabled={false}
 				onChange={onChange}
+				value={values}
 			/>
-			<CaptureButton labels={['']} onChange={(_, pin) => onSelect(pin)} small={true} />
+			<CaptureButton
+				labels={['']}
+				onChange={(_, pin) => addPin(pinsKey, pin)}
+				small={true}
+			/>
 		</div>
 	);
 }
@@ -91,7 +106,7 @@ type ProfileOption = {
 	disabled: boolean;
 };
 
-function ProfileSelect() {
+function ProfileSelect({ mappingIndex }: { mappingIndex: number }) {
 	const profiles: ProfileOption[] = useProfilesStore((state) =>
 		state.profiles.map((p, i) => ({
 			label: p.profileLabel,
@@ -99,12 +114,21 @@ function ProfileSelect() {
 			disabled: !p.enabled,
 		})),
 	);
+	const profileIndex = useBootModesStore(
+		(state) => state.bootModes[mappingIndex].profileIndex,
+	);
+	const setProfileIndex = useBootModesStore((state) => state.setProfileIndex);
+	const value = profiles.find(({ value }) => value === profileIndex);
 
 	const getLabel = (option: ProfileOption) =>
 		option.label + (option.disabled ? ' (Disabled)' : '');
 
 	const onChange = (_selected: any, action: ActionMeta<ProfileOption>) => {
-		console.log(action);
+		if (action.action == 'clear') {
+			setProfileIndex(mappingIndex, undefined);
+		} else if (action.action == 'select-option') {
+			setProfileIndex(mappingIndex, action.option?.value);
+		}
 	};
 
 	return (
@@ -116,11 +140,12 @@ function ProfileSelect() {
 			isOptionDisabled={(option) => option.disabled}
 			onChange={onChange}
 			placeholder="Last Used"
+			value={value}
 		/>
 	);
 }
 
-function BootModeRow({
+function FormRow({
 	col0,
 	col1,
 	col2,
@@ -141,20 +166,33 @@ function BootModeRow({
 	);
 }
 
-export default function BootModeMappingPage() {
+function BootModeRow({ mappingIndex }: { mappingIndex: number }) {
+	const removeBootMode = useBootModesStore((state) => state.removeBootMode);
+
+	return (
+		<FormRow
+			col0={<BootModeSelect mappingIndex={mappingIndex} />}
+			col1={<PinSelect pinsKey={mappingIndex} />}
+			col2={<ProfileSelect mappingIndex={mappingIndex} />}
+			col3={
+				<Button
+					onClick={() => {
+						removeBootMode(mappingIndex);
+					}}
+				>
+					{'✕'}
+				</Button>
+			}
+		/>
+	);
+}
+
+export default function BootModeMapping() {
 	const loadingBootModes = useBootModesStore((state) => state.loadingBootModes);
 	const loadingProfiles = useProfilesStore((state) => state.loadingProfiles);
 
 	const addBootMode = useBootModesStore((state) => state.addBootMode);
-	const removeBootMode = useBootModesStore((state) => state.removeBootMode);
 	const bootModes = useBootModesStore((state) => state.bootModes);
-
-	const addWebConfigPin = useBootModesStore((state) => state.addWebConfigPin);
-	const removeWebConfigPin = useBootModesStore((state) => state.removeWebConfigPin);
-	const addUsbModePin = useBootModesStore((state) => state.addUsbModePin);
-	const removeUsbModePin = useBootModesStore((state) => state.removeUsbModePin);
-	const addPin = useBootModesStore((state) => state.addPin);
-	const clearPin = useBootModesStore((state) => state.removePin);
 
 	return (
 		<div id="BootModeSelect">
@@ -164,55 +202,36 @@ export default function BootModeMappingPage() {
 				</div>
 			) : (
 				<Container fluid className="p-0">
-					<BootModeRow
+					<FormRow
 						col0={<Form.Text className="muted ms-2">MODE</Form.Text>}
 						col1={<Form.Text className="muted ms-2">GPIO PINS</Form.Text>}
 						col2={<Form.Text className="muted ms-2">PROFILE</Form.Text>}
 						col3={<Button className="invisible">{'✕'}</Button>}
 					/>
 					<hr />
-					<BootModeRow
+					<FormRow
 						col0={<label className="ms-2">Web-Config</label>}
-						col1={<PinSelect onSelect={addWebConfigPin} onRemove={removeWebConfigPin} />}
+						col1={<PinSelect pinsKey="webConfigPins" />}
 						col2={<CustomSelect isDisabled={true} placeholder="N/A" />}
 						col3={<Button disabled={true}>{'✕'}</Button>}
 					/>
-					<BootModeRow
+					<FormRow
 						col0={<label className="ms-2">USB (BOOTSEL)</label>}
-						col1={<PinSelect onSelect={addUsbModePin} onRemove={removeUsbModePin} />}
+						col1={<PinSelect pinsKey="usbModePins" />}
 						col2={<CustomSelect isDisabled={true} placeholder="N/A" />}
 						col3={<Button disabled={true}>{'✕'}</Button>}
 					/>
 					{bootModes.map((mode, index) => (
 						<div key={`boot-mode-${mode.key}`}>
-							<BootModeRow
-								col0={<BootModeSelect mappingIndex={index} />}
-								col1={
-									<PinSelect
-										onSelect={(pin: number) => addPin(index, pin)}
-										onRemove={(pin: number) => clearPin(index, pin)}
-									/>
-								}
-								col2={<ProfileSelect />}
-								col3={
-									<Button
-										onClick={() => {
-											removeBootMode(index);
-										}}
-									>
-										{'✕'}
-									</Button>
-								}
-							/>
+							<BootModeRow mappingIndex={index} />
 						</div>
 					))}
+
 					{bootModes.length < 8 && (
-						<div className="d-flex flex-row gap-2 justify-content-end">
-							<div className="d-flex flex-grow-1 justify-content-center">
-								<Button className="mt-1" variant="outline" onClick={addBootMode}>
-									+ Add Mode
-								</Button>
-							</div>
+						<div className="d-flex justify-content-center">
+							<Button className="mt-1" variant="outline" onClick={addBootMode}>
+								+ Add Mode
+							</Button>
 						</div>
 					)}
 				</Container>
