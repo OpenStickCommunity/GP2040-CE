4272a92170444b7a1deac3e34dc7168b584c4125
Podbod12 <pod@podrasoft.com>
2026-04-07 19:56:15 +0100

    Update BoardConfig.h

--- configs/FlatboxRev5RGB/BoardConfig.h
+++ configs/FlatboxRev5RGB/BoardConfig.h
@@ -62,8 +62,24 @@
 #define BOARD_LEDS_PIN 0
 
 #define LED_BRIGHTNESS_MAXIMUM 50
-#define LEDS_BASE_ANIMATION_INDEX AnimationNonPressedEffects::AnimationNonPressedEffects_EFFECT_RAINBOW_SYNCED
-#define LEDS_PER_PIXEL 2
+
+#define BOARD_LEDS_PIN 0
+#define LIGHT_DATA_NAME_DEFAULT "Flatbox Rev5 RGB" 
+#define LIGHT_DATA_SIZE_DEFAULT 12 //number of sets in the below data
+#define LEDS_BASE_ANIMATION_INDEX AnimationNonPressedEffects::AnimationNonPressedEffects_EFFECT_RAINBOW_ROTATE
+#define LIGHT_DATA_DEFAULT \
+0,  2, 0,  3, 13, LightType::LightType_ActionButton, \
+2,  2, 2,  3, 12, LightType::LightType_ActionButton, \
+4,  2, 4,  4, 11, LightType::LightType_ActionButton, \
+6,  2, 6,  3, 10, LightType::LightType_ActionButton, \
+8,  2, 8,  2, 9,  LightType::LightType_ActionButton, \
+10, 2, 10, 2, 8,  LightType::LightType_ActionButton, \
+12, 2, 12, 2, 7,  LightType::LightType_ActionButton, \
+14, 2, 12, 4, 6,  LightType::LightType_ActionButton, \
+16, 2, 10, 4, 5,  LightType::LightType_ActionButton, \
+18, 2, 8,  4, 4,  LightType::LightType_ActionButton, \
+20, 2, 6,  5, 3,  LightType::LightType_ActionButton, \
+22, 2, 5,  7, 2,  LightType::LightType_ActionButton, 
 
 #define LEDS_DPAD_LEFT   0
 #define LEDS_DPAD_DOWN   1
